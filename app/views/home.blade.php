49fc4d1cd0d9606cc3dd9735be9eefa5c8c2591f
Matt Stauffer <matt@mattstaufferdesign.com>
2013-12-03 23:43:49 -0800

    Update front page content.

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -4,13 +4,13 @@
 
 <div class="hero">
 	<div class="container">
-		<h1>Hello, world!</h1>
-		<p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-		<p><a class="button button--primary button--large" role="button">Learn more &raquo;</a></p>
+		<h1>Style your markdown!</h1>
+		<p>Markedstyle is a collection of CSS files for styling the <a href="http://marked2app.com/">Marked</a> markdown preview app.</p>
+		<p><a href="/styles" class="button button--primary button--large" role="button">Browse Styles &raquo;</a></p>
 	</div>
 </div>
 	
-<div class="container">
+<div class="container" style="display: none;">
 	<div class="row">
 		<div class="col-third">
 			<h2>Heading</h2>
