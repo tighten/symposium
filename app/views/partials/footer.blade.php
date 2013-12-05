2c5d61ea4ff50f61e19f124b11e91a3b822f68bf
Matt Stauffer <matt@mattstaufferdesign.com>
2013-12-05 11:00:47 -0800

    Update footer.

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -2,6 +2,6 @@
 	<hr>
 
 	<footer>
-	<p>&copy; Markedstyle {{ date('Y') }} - by <a href="http://mattstauffer.co/">Matt Stauffer</a>, powered by <a href="http://www.laravel.com/">Laravel</a></p>
+	<p>&copy; Markedstyle {{ date('Y') }} | written by <a href="http://mattstauffer.co/">Matt Stauffer</a> | powered by <a href="http://www.laravel.com/">Laravel</a> | source on <a href="https://github.com/mattstauffer/markedstyle">Github</a></p>
 	</footer>
 </div>
