ae06c390c33daa7c15e84e7c1bf8f9442d87e9dc
Matt Stauffer <matt@mattstaufferdesign.com>
2013-12-06 22:45:15 -0500

    Unknown amount of work towards creating new styles. This should be a branch. For shame.

--- app/views/partials/nav.blade.php
+++ app/views/partials/nav.blade.php
@@ -1,6 +1,6 @@
 <nav>
 	<ul class="primary-header__meta-nav">
-		<li><a href="/">Home</a></li>
+		<li><a href="/styles/create">New Style</a></li>
 		<li><a href="/styles">Browse Styles</a></li>
 		@if (Auth::check())
 		<li><a href="/account">Account</a></li>
--- app/views/styles/create.blade.php
+++ app/views/styles/create.blade.php
@@ -5,13 +5,10 @@
 <div class="container">
 	<h1>Create Style</h1>
 
-	{{ Form::open(array('action' => 'StylesController@store')) }}
+	{{ Form::open(array('action' => 'StylesController@store', 'class' => 'new-style-form')) }}
 
 	{{ Form::label('title', 'Title') }}<br>
 	{{ Form::text('title') }}<br><br>
-
-	{{ Form::label('slug', 'Slug') }}<br>
-	{{ Form::text('slug') }}<br><br>
 
 	{{ Form::label('description', 'Description') }}<br>
 	{{ Form::textarea('description') }}<br><br>
@@ -21,8 +18,8 @@
 
 	{{ Form::label('format', 'Style Type') }}<br>
 	{{ Form::radio('format', 'css', true) }} CSS
-	{{ Form::radio('format', 'scss') }} SCSS
-	{{ Form::radio('format', 'sass') }} Sass<br><br>
+	{{ Form::radio('format', 'scss', false, array('disabled' => true)) }} SCSS
+	{{ Form::radio('format', 'sass', false, array('disabled' => true)) }} Sass<br><br>
 
 	{{ Form::submit('Create') }}<br><br>
 
