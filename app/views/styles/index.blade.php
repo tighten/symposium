f94574f5c98e16a82af881c39552d89486f77d4a
Matt Stauffer <register@thisenddown.com>
2013-12-16 00:27:57 -0500

    Round popularity.

--- app/views/styles/index.blade.php
+++ app/views/styles/index.blade.php
@@ -9,7 +9,7 @@
 	<ul class="list-styles">
 		@foreach($styles as $style)
 		<li><h3><a href="/styles/{{ $style->slug }}">{{ $style->title }}</a></h3>
-			<p class="style-meta">By <a href="/authors/{{ $style->author->id }}">{{ $style->author->first_name }} {{ $style->author->last_name }}</a> - <i>{{ $style->created_at->toFormattedDateString()  }}</i> - popularity {{ $style->visits / $total_visits * 100 }}</p></li>
+			<p class="style-meta">By <a href="/authors/{{ $style->author->id }}">{{ $style->author->first_name }} {{ $style->author->last_name }}</a> - <i>{{ $style->created_at->toFormattedDateString()  }}</i> - popularity {{ round($style->visits / $total_visits * 100) }}</p></li>
 		@endforeach
 	</ul>
 </div>
