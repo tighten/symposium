aeac76f561e68ff1aa541486ae516026bdff38d1
Matt Stauffer <matt@tighten.co>
2014-12-20 14:14:32 -0500

    Bugsnag only on prod

--- app/config/packages/bugsnag/bugsnag-laravel/config.php
+++ app/config/packages/bugsnag/bugsnag-laravel/config.php
@@ -3,5 +3,6 @@
      * Set your Bugsnag API Key.
      * You can find your API Key on your Bugsnag dashboard.
      */
-	'api_key' => 'e064c41b63dcdc3e5005dab44005ea2c'
+	'api_key' => 'e064c41b63dcdc3e5005dab44005ea2c',
+	'notify_release_stages' => ['production', 'staging']
 );
