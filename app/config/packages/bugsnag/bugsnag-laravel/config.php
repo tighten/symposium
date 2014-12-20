a2ab8652cb9a09267cf562d06defc3428ad58863
Matt Stauffer <matt@tighten.co>
2014-12-20 14:33:55 -0500

    Update bugsnag creds

--- app/config/packages/bugsnag/bugsnag-laravel/config.php
+++ app/config/packages/bugsnag/bugsnag-laravel/config.php
@@ -3,6 +3,6 @@
      * Set your Bugsnag API Key.
      * You can find your API Key on your Bugsnag dashboard.
      */
-	'api_key' => 'e064c41b63dcdc3e5005dab44005ea2c',
+	'api_key' => '7a63af3711062295dff0009b994fc20c',
 	'notify_release_stages' => ['production', 'staging']
 );
