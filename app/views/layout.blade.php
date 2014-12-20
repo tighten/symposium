d7197ef678b60e8e731c812f61760613094ade96
Matt Stauffer <matt@tighten.co>
2014-12-20 01:35:17 -0500

    Drop stylesheet() from jeff way guard

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		{{ stylesheet() }}
 		<title>SaveMyProposals</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="/css/app.css" rel="stylesheet">
 		<!--[if lt IE 9]>
 			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 			<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
