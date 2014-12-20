f6a83afdc946fa670a4c3397d930850b14e39314
Matt Stauffer <matt@tighten.co>
2014-12-20 01:36:20 -0500

    Drop script() from jeff way guard

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -14,8 +14,8 @@
 		@yield('content')
 		@include('partials.footer')
 
-		{{ script('vendor/jquery-2.0.3.min.js') }}
-		{{ script('main.js') }}
+		<script href="/js/vendor/jquery-2.0.3.min.js"></script>
+		<script href="/js/main.js"></script>
 
 		<script>
 		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
