31e726818320d344099ba0a70dadc349b95523a1
Matt Stauffer <matt@tighten.co>
2014-12-20 21:16:47 -0500

    Fix mobile style and embarrassing js typo.

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -14,8 +14,9 @@
 		@yield('content')
 		@include('partials.footer')
 
-		<script href="/js/vendor/jquery-2.0.3.min.js"></script>
-		<script href="/js/main.js"></script>
+		<script src="//cdn.jsdelivr.net/jquery/2.1.3/jquery.min.js"></script>
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+		<script src="/js/main.js"></script>
 
 		<script>
 		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
