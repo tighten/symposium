78813cc44e1dcb2aa19cab6f7fa2c3a921ec7955
Matt Stauffer <matt@tighten.co>
2015-01-05 12:16:05 -0500

    Fix count on talk versions

--- app/views/talks/show.blade.php
+++ app/views/talks/show.blade.php
@@ -21,22 +21,21 @@
 
         <h2>Versions</h2>
         <ul>
-            @if (empty($talk->versions))
+            @if (count($talk->versions) == 0)
                 <li>No versions yet. <a href="/talks/{{ $talk->id }}/createVersion" class="btn btn-primary btn-sm">Add one</a><br>
                     <i>Each talk can have one or more "versions"--think about, for example, the difference between a "lightning" talk or a "keynote." Or maybe the difference between the talk as you'd propose it to one group of people vs. another.</i></li>
-                @else
-
-        @foreach ($talk->versions as $version)
-            <li>
-                <a href="/talks/{{ $talk->id }}/versions/{{ $version->id }}/delete"><button type="button" class="btn btn-xs btn-danger">Delete</button></a>
-                <a href="/talks/{{ $talk->id }}/versions/{{ $version->id }}/edit"><button type="button" class="btn btn-xs btn-default">Edit</button></a> -
-                <a href="/talks/{{ $talk->id }}/versions/{{ $version->id }}">{{ $version->nickname }}</a>
-                @if ($version->current())
-                    <i>{{ $version->current()->length }}-minute {{ $version->current()->type }}</i>
-                @endif
-                </li>
-        @endforeach
-                @endif
+            @else
+                @foreach ($talk->versions as $version)
+                    <li>
+                        <a href="/talks/{{ $talk->id }}/versions/{{ $version->id }}/delete"><button type="button" class="btn btn-xs btn-danger">Delete</button></a>
+                        <a href="/talks/{{ $talk->id }}/versions/{{ $version->id }}/edit"><button type="button" class="btn btn-xs btn-default">Edit</button></a> -
+                        <a href="/talks/{{ $talk->id }}/versions/{{ $version->id }}">{{ $version->nickname }}</a>
+                        @if ($version->current())
+                            <i>{{ $version->current()->length }}-minute {{ $version->current()->type }}</i>
+                        @endif
+                    </li>
+                @endforeach
+            @endif
         </ul>
 
     </div>
