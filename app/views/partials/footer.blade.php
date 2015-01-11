4f62a41320bb0177d49f63731c237de35bce071f
Matt Stauffer <matt@tighten.co>
2015-01-11 16:05:01 -0500

    Add waffle.io link to footer

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -2,8 +2,10 @@
     <hr>
 
     <footer>
-        <p>&copy; SaveMyProposals {{ date('Y') }} | written by <a href="http://mattstauffer.co/">Matt Stauffer</a> |
-            powered by <a href="http://www.laravel.com/">Laravel</a> | source on <a
-                    href="https://github.com/mattstauffer/savemyproposals">Github</a></p>
+        <p>&copy; SaveMyProposals {{ date('Y') }}
+            | written by <a href="http://mattstauffer.co/">Matt Stauffer</a>
+            | powered by <a href="http://www.laravel.com/">Laravel</a>
+            | source on <a href="https://github.com/mattstauffer/savemyproposals">Github</a>
+            | roadmap/bugs on <a href="https://waffle.io/mattstauffer/savemyproposals">Waffle.io</a></p>
     </footer>
 </div>
