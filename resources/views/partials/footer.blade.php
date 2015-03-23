0863297425ac0bd083eb5180b82ca736568cfefa
Matt Stauffer <matt@tighten.co>
2015-03-23 09:12:23 -0400

    Fix footer links to correct URL

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,8 +3,8 @@
 
     <footer>
         <p>&copy; <a href="http://tighten.co/">Tighten Co.</a> {{ date('Y') }}
-            | source on <a href="https://github.com/mattstauffer/savemyproposals">Github</a>
-            | roadmap/bugs on <a href="https://waffle.io/mattstauffer/savemyproposals">Waffle.io</a></p>
+            | source on <a href="https://github.com/tightenco/symposium">Github</a>
+            | roadmap/bugs on <a href="https://waffle.io/tightenco/symposium">Waffle.io</a></p>
         </p>
     </footer>
 </div>
