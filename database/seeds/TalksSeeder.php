3e7029f0967ff000ff341201ca32b163168b6036
Adam Wathan <adam.wathan@gmail.com>
2015-05-08 08:36:52 -0400

    Truncate using models

--- database/seeds/TalksSeeder.php
+++ database/seeds/TalksSeeder.php
@@ -6,9 +6,9 @@ class TalksSeeder extends Seeder
 {
     public function run()
     {
-        DB::table('talk_version_revisions')->truncate();
-        DB::table('talk_versions')->truncate();
-        DB::table('talks')->truncate();
+        TalkVersionRevision::truncate();
+        TalkVersion::truncate();
+        Talk::truncate();
 
         $talks = [
             [
