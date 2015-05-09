66d9545d6df0810ba021ab85473549d5ec0e99cc
Matt Stauffer <matt@tighten.co>
2015-05-09 12:35:33 -0400

    Add copy bio button

--- resources/views/bios/index.blade.php
+++ resources/views/bios/index.blade.php
@@ -21,6 +21,29 @@
                     <h3><a href="/bios/{{ $bio->id }}">{{ $bio->nickname }}</a></h3>
                     <?php /* TODO: cleaner substr */ ?>
                     <p>{{ substr($bio->body, 0, 100) }}...</p>
+
+                    <div class="modal fade bio-modal" id="modal-{{ $bio->id }}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                      <div class="modal-dialog">
+                        <div class="modal-content">
+                          <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                            <h4 class="modal-title" id="myModalLabel">Copy bio</h4>
+                          </div>
+                          <div class="modal-body">
+                            <textarea class="select-me" style="width: 100%; height: 10em;">{{ str_replace("\n", "<br>", $bio->body) }}</textarea>
+                          </div>
+                          <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    <p>
+                    <a href="/bios/{{ $bio->id }}/delete" data-confirm="confirm"><button type="button" class="btn btn-xs btn-danger">Delete</button></a>
+                    <a href="/bios/{{ $bio->id }}/edit"><button type="button" class="btn btn-xs btn-default">Edit</button></a>
+                    <a href="#" data-toggle="modal" data-target="#modal-{{ $bio->id }}"><button type="button" class="btn btn-xs btn-default">Copy</button></a>
+                    </p>
                 </li>
             @endforeach
         </ul>
--- resources/views/bios/show.blade.php
+++ resources/views/bios/show.blade.php
@@ -9,18 +9,16 @@
             <li class="active"><a href="/bios/{{ $bio->id }}">Bio: {{ $bio->nickname }}</a></li>
         </ol>
 
-        <h1>{{ $bio->nickname }}</h1>
-
         <p class="pull-right">
             <a href="/bios/{{ $bio->id }}/edit" class="btn btn-default">Edit &nbsp;<span class="glyphicon glyphicon-edit" aria-hidden="true"></span></a>
             <a href="/bios/{{ $bio->id }}/delete" class="btn btn-danger">Delete &nbsp;<span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a>
         </p>
 
-        <p><b>Date created:</b>
-            {{ $bio->created_at->toFormattedDateString() }}</p>
+        <h1>{{ $bio->nickname }}</h1>
 
-        <p><b>Body:</b><br>
-            <!-- TODO: Figure out how we will be handling HTML/etc. -->
+        <p class="bio-meta">Created {{ $bio->created_at->toFormattedDateString() }} | Updated {{ $bio->updated_at->toFormattedDateString() }}</p>
+
+        <p><!-- TODO: Figure out how we will be handling HTML/etc. -->
             {{ str_replace("\n", "<br>", $bio->body) }}</p>
 
     </div>
