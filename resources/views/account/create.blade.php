07c482510b66ca85a2c2cda3285a58a245c97aad
Matt Stauffer <matt@tighten.co>
2015-05-18 09:58:02 -0400

    Bring back signup/login labels but sr-only

--- resources/views/account/create.blade.php
+++ resources/views/account/create.blade.php
@@ -19,21 +19,25 @@
 
                     {{ Form::open(['action' => ['AccountController@store']]) }}
                     <div class="form-group">
+                        {{ Form::label('email', '*Email Address', ['class' => 'sr-only']) }}
                         {{ Form::email('email', null, ['autofocus' => 'autofocus', 'class' => 'form-control', 'placeholder' => 'Email address']) }}
                     </div>
 
                     <div class="form-group">
+                        {{ Form::label('password', '*Password', ['class' => 'sr-only']) }}
                         {{ Form::password('password', ['class' => 'form-control', 'placeholder' => 'Password']) }}
                     </div>
 
                     <div class="row">
                         <div class="col-md-6">
                             <div class="form-group">
+                                {{ Form::label('first_name', '*First Name', ['class' => 'sr-only']) }}
                                 {{ Form::text('first_name', null, ['class' => 'form-control', 'placeholder' => 'First name']) }}
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
+                                {{ Form::label('last_name', '*Last Name', ['class' => 'sr-only']) }}
                                 {{ Form::text('last_name', null, ['class' => 'form-control', 'placeholder' => 'Last name']) }}
                             </div>
                         </div>
--- resources/views/user/log-in.blade.php
+++ resources/views/user/log-in.blade.php
@@ -11,9 +11,11 @@
                     <div class="panel-body">
                         {{ Form::open(['action' => 'AuthController@postLogin']) }}
                         <div class="form-group">
+                            {{ Form::label('email', 'Email', ['class' => 'sr-only']) }}
                             {{ Form::text('email', null, ['autofocus' => 'autofocus', 'class' => 'form-control', 'placeholder' => 'Email address']) }}
                         </div>
                         <div class="form-group">
+                            {{ Form::label('password', 'Password', ['class' => 'sr-only']) }}
                             {{ Form::password('password', ['class' => 'form-control', 'placeholder' => 'Password']) }}
                         </div>
 
