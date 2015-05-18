0ff608290f1fa0068682bf56d327affeef627fdd
Matt Stauffer <matt@tighten.co>
2015-05-17 23:58:20 -0400

    Update style for login and signup forms.

--- resources/views/account/create.blade.php
+++ resources/views/account/create.blade.php
@@ -3,40 +3,46 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-6">
-                <h2>Sign up</h2>
-
-                <ul class="errors">
-                    @foreach ($errors->all() as $message)
-                        <li>{{ $message }}</li>
-                    @endforeach
-                </ul>
-
-                {{ Form::open(array('action' => array('AccountController@store'))) }}
-
-                <div class="form-group">
-                    {{ Form::label('email', '*Email Address', ['class' => 'control-label']) }}
-                    {{ Form::email('email', null, ['autofocus' => 'autofocus', 'class' => 'form-control']) }}
+            <div class="col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h2 class="panel-title">Sign up</h2>
                 </div>
+                <div class="panel-body">
+                    @if (! $errors->isEmpty())
+                    <ul class="errors">
+                        @foreach ($errors->all() as $message)
+                            <li>{{ $message }}</li>
+                        @endforeach
+                    </ul>
+                    @endif
 
-                <div class="form-group">
-                    {{ Form::label('password', '*Password', ['class' => 'control-label']) }}
-                    {{ Form::password('password', ['class' => 'form-control']) }}
+                    {{ Form::open(array('action' => array('AccountController@store'))) }}
+                    <div class="form-group">
+                        {{ Form::email('email', null, ['autofocus' => 'autofocus', 'class' => 'form-control', 'placeholder' => 'Email address']) }}
+                    </div>
+
+                    <div class="form-group">
+                        {{ Form::password('password', ['class' => 'form-control', 'placeholder' => 'Password']) }}
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                {{ Form::text('first_name', null, ['class' => 'form-control', 'placeholder' => 'First name']) }}
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                {{ Form::text('last_name', null, ['class' => 'form-control', 'placeholder' => 'Last name']) }}
+                            </div>
+                        </div>
+                    </div>
+
+                    {{ Form::submit('Sign up', array('class' => 'button button--primary')) }}
+
+                    {{ Form::close() }}
                 </div>
-
-                <div class="form-group">
-                    {{ Form::label('first_name', '*First Name', ['class' => 'control-label']) }}
-                    {{ Form::text('first_name', null, ['class' => 'form-control']) }}
-                </div>
-
-                <div class="form-group">
-                    {{ Form::label('last_name', '*Last Name', ['class' => 'control-label']) }}
-                    {{ Form::text('last_name', null, ['class' => 'form-control']) }}
-                </div>
-
-                {{ Form::submit('Save', array('class' => 'button button--primary')) }}
-
-                {{ Form::close() }}
             </div>
         </div>
     </div>
--- resources/views/user/log-in.blade.php
+++ resources/views/user/log-in.blade.php
@@ -3,22 +3,24 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-6">
-                {{ Form::open(array('action' => 'AuthController@postLogin')) }}
-                <h2>Log in</h2>
+            <div class="col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h2 class="panel-title">Log in</h2>
+                    </div>
+                    <div class="panel-body">
+                        {{ Form::open(array('action' => 'AuthController@postLogin')) }}
+                        <div class="form-group">
+                            {{ Form::text('email', null, ['autofocus' => 'autofocus', 'class' => 'form-control', 'placeholder' => 'Email address']) }}
+                        </div>
+                        <div class="form-group">
+                            {{ Form::password('password', ['class' => 'form-control', 'placeholder' => 'Password']) }}
+                        </div>
 
-                <div class="form-group">
-                    {{ Form::label('email', 'Email', ['class' => 'control-label']) }}<br>
-                    {{ Form::text('email', null, ['autofocus' => 'autofocus', 'class' => 'form-control']) }}
+                        {{ Form::submit('Sign in', array('class' => 'button button--primary')) }}
+                        {{ Form::close() }}
+                    </div>
                 </div>
-                <div class="form-group">
-                    {{ Form::label('password', 'Password', ['class' => 'control-label']) }}<br>
-                    {{ Form::password('password', ['class' => 'form-control']) }}
-                </div>
-                <br>
-
-                {{ Form::submit('Sign in', array('class' => 'button button--primary')) }}
-                {{ Form::close() }}
             </div>
         </div>
     </div>
