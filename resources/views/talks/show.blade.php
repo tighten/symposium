a8d67668bd049f050a05dcda8a54171eb43323e1
Matt Stauffer <matt@tighten.co>
2015-06-06 14:04:07 -0400

    Convert revision url to use query params

--- resources/views/talks/show.blade.php
+++ resources/views/talks/show.blade.php
@@ -9,7 +9,7 @@
             <li><a href="/talks/{{ $talk->id }}">Talk: {{ $current->title }}</a></li>
         </ol>
 
-        @if ($talk->current()->id == $current->id)
+        @if (! $showingRevision)
         <p class="pull-right">
             <a href="/talks/{{ $talk->id }}/edit" class="btn btn-primary">Edit &nbsp;<span class="glyphicon glyphicon-edit" aria-hidden="true"></span></a>
             <a href="/talks/{{ $talk->id }}/delete" class="btn btn-danger">Delete &nbsp;<span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a>
@@ -20,8 +20,8 @@
             <p>No revisions yet. @todo Throw exception etc.</p>
         @else
 
-        @if ($talk->current()->id != $current->id)
-            REVISION: {{ $current->created_at }} | <a href="/talks/{{ $talk->id }}">Return to current revision</a>
+        @if ($showingRevision)
+            REVISION: {{ $current->created_at }} | <a href="/talks/{{ $talk->id }}">Return to talk</a>
         @endif
 
         <h1>{{ $current->title }}</h1>
@@ -47,7 +47,7 @@
                 <ul>
                     @foreach ($talk->revisions as $revision)
                         <li {{ $revision->id == $current->id ? 'style="font-weight: bold;"' : '' }}>
-                            <a href="/talks/{{ $talk->id }}/revisions/{{ $revision->id }}">{{ $revision->created_at }}</a> {{ $talk->current()->id == $revision->id ? '<i>(current)</i>' : '' }}
+                            <a href="/talks/{{ $talk->id }}?revision={{ $revision->id }}">{{ $revision->created_at }}</a> {{ $talk->current()->id == $revision->id ? '<i>(current)</i>' : '' }}
                         </li>
                     @endforeach
                 </ul>
