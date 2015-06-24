c21258e4b09695e8ead9fdc66a9f0c69d3179082
Matt Stauffer <matt@tighten.co>
2015-06-23 21:14:17 -0400

    Tweak tests to handle oAuth

---
+++ app/ApiResources/Conference.php
@@ -0,0 +1,46 @@
+<?php namespace Symposium\ApiResources;
+
+use Conference as EloquentConference;
+use Illuminate\Contracts\Support\Arrayable;
+
+class Conference implements Arrayable
+{
+    private $conference;
+
+    public function __construct(EloquentBio $conference)
+    {
+        $this->conference = $conference;
+    }
+
+    public function getId()
+    {
+        return $this->conference->id;
+    }
+
+    public function getType()
+    {
+        return 'conferences';
+    }
+
+    public function attributes()
+    {
+        return [
+            'title' => $this->conference->title,
+            'description' => $this->conference->description,
+            'url' => $this->conference->url,
+            'starts_at' => (string)$this->conference->starts_at,
+            'ends_at' => (string)$this->conference->ends_at,
+            'cfp_starts_at' => (string)$this->conference->cfp_starts_at,
+            'cfp_ends_at' => (string)$this->conference->cfp_ends_at,
+        ];
+    }
+
+    public function toArray()
+    {
+        return [
+            'id' => $this->getId(),
+            'type' => $this->getType(),
+            'attributes' => $this->attributes()
+        ];
+    }
+}
--- tests/Api/ApiTestCase.php
+++ tests/Api/ApiTestCase.php
@@ -1,5 +1,8 @@
 <?php
 
+use LucaDegasperi\OAuth2Server\Authorizer;
+use LucaDegasperi\OAuth2Server\Filters\OAuthFilter;
+
 class ApiTestCase extends TestCase
 {
     public function setUp()
@@ -9,9 +12,13 @@ class ApiTestCase extends TestCase
         Artisan::call('migrate');
         Artisan::call('db:seed');
 
-        Auth::loginUsingId(1);
+        // Auth::loginUsingId(1);
         // OR:
         // $this->be(User::find(1));
+
+        $userId = 1;
+
+        $this->authenticateAs($userId);
     }
 
     protected function parseJson($response)
@@ -22,5 +29,53 @@ class ApiTestCase extends TestCase
     protected function assertIsJson($data)
     {
         $this->assertEquals(0, json_last_error(), 'Return is not JSON');
+    }
+
+    protected function authenticateAs($userId)
+    {
+        $this->app->singleton(Authorizer::class, function () use ($userId) {
+            $authorizer = new DummyAuthorizer;
+            $authorizer->userId = $userId;
+            return $authorizer;
+        });
+
+        $this->app->singleton(OAuthFilter::class, function () use ($userId) {
+            return new DummyOAuthFilter;
+        });
+    }
+}
+
+
+class DummyAuthorizer extends Authorizer
+{
+    public $userId;
+
+    public function __construct()
+    {
+    }
+
+    public function getResourceOwnerId()
+    {
+        return $this->userId;
+    }
+
+    public function getResourceOwnerType()
+    {
+        return 'user';
+    }
+
+    public function getSession()
+    {
+    }
+}
+
+class DummyOAuthFilter extends OAuthFilter
+{
+    public function __construct()
+    {
+    }
+
+    public function filter()
+    {
     }
 }
