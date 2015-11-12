54f76751790128d3ad48a0c09183390ed3a0098f
Jason Walton <jwalton512@gmail.com>
2015-11-12 14:36:03 -0700

    Add edit/delete links to talk list page

--- resources/views/partials/talk-in-list.blade.php
+++ resources/views/partials/talk-in-list.blade.php
@@ -1,2 +1,6 @@
+<div class="pull-right">
+	<a href="/talks/{{ $talk->id }}/edit" class="btn btn-sm btn-primary">Edit &nbsp;<span class="glyphicon glyphicon-edit" aria-hidden="true"></span></a>
+	<a href="/talks/{{ $talk->id }}/delete" class="btn btn-sm btn-danger">Delete &nbsp;<span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a>
+</div>
 <h3><a href="{{ route('talks.show', ['id' => $talk->id]) }}">{{ $talk->current()->title }}</a></h3>
 <p class="talk-meta"><i>{{ $talk->created_at->toFormattedDateString()  }}</i> | {{ $talk->current()->length }}-minute {{ $talk->current()->level }} {{ $talk->current()->type }}</p>
