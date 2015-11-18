81da1d71db0b30c8f137135bbc13866d8289f161
Matt Stauffer <matt@tighten.co>
2015-11-17 21:39:05 -0500

    Tweak styles

--- resources/views/partials/talk-in-list.blade.php
+++ resources/views/partials/talk-in-list.blade.php
@@ -1,6 +1,12 @@
 <div class="pull-right">
-	<a href="/talks/{{ $talk->id }}/edit" class="btn btn-sm btn-primary">Edit &nbsp;<span class="glyphicon glyphicon-edit" aria-hidden="true"></span></a>
-	<a href="/talks/{{ $talk->id }}/delete" class="btn btn-sm btn-danger">Delete &nbsp;<span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a>
+    <a href="{{ route('talks.delete', ['id' => $talk->id]) }}" class="btn btn-xs btn-danger">
+        <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+        Delete
+    </a>
+    <a href="{{ route('talks.edit', ['id' => $talk->id]) }}" class="btn btn-xs btn-primary">
+        <span class="glyphicon glyphicon-edit" aria-hidden="true"></span>
+        Edit
+    </a>
 </div>
 <h3><a href="{{ route('talks.show', ['id' => $talk->id]) }}">{{ $talk->current()->title }}</a></h3>
 <p class="talk-meta"><i>{{ $talk->created_at->toFormattedDateString()  }}</i> | {{ $talk->current()->length }}-minute {{ $talk->current()->level }} {{ $talk->current()->type }}</p>
