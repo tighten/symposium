0d2801104bc2b90e76dc0546d6de920cf6a64483
Matt Stauffer <matt@tighten.co>
2015-11-23 21:20:47 -0500

    Composer update, and pull in exception handler and test case fresh from laravel/framework

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -11,11 +11,11 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase {
 	 */
 	public function createApplication()
 	{
-		$app = require __DIR__.'/../bootstrap/app.php';
+        $app = require __DIR__.'/../bootstrap/app.php';
 
-		$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
+        $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
-		return $app;
+        return $app;
 	}
 
 }
