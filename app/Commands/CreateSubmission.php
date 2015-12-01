920b14f89b883cf5beac178890cae70317775ebf
Matt Stauffer <matt@tighten.co>
2015-11-30 20:45:00 -0500

    Tabs to spaces in CreateSubmission

--- app/Commands/CreateSubmission.php
+++ app/Commands/CreateSubmission.php
@@ -8,21 +8,21 @@ use TalkRevision;
 
 class CreateSubmission extends Command implements SelfHandling
 {
-	private $conferenceId;
-	private $talkId;
+    private $conferenceId;
+    private $talkId;
 
-	public function __construct($conferenceId, $talkId)
-	{
-		$this->conferenceId = $conferenceId;
-		$this->talkId = $talkId;
-	}
+    public function __construct($conferenceId, $talkId)
+    {
+        $this->conferenceId = $conferenceId;
+        $this->talkId = $talkId;
+    }
 
-	public function handle()
-	{
-		$conference = Conference::findOrFail($this->conferenceId);
-		$talk = Talk::findOrFail($this->talkId);
-		$conference->submissions()->save($talk->current(), [
-			'status' => 'submitted'
-		]);
-	}
+    public function handle()
+    {
+        $conference = Conference::findOrFail($this->conferenceId);
+        $talk = Talk::findOrFail($this->talkId);
+        $conference->submissions()->save($talk->current(), [
+            'status' => 'submitted'
+        ]);
+    }
 }
