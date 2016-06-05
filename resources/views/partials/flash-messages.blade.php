380a18d54da748b1edd023cba69e046c42720e80
Matt Stauffer <matt@tighten.co>
2016-06-05 09:25:37 -0400

    allow for manually dismissing alerts. 1 step towards #107

--- resources/views/partials/flash-messages.blade.php
+++ resources/views/partials/flash-messages.blade.php
@@ -1,17 +1,17 @@
 @if (Session::has('success-message') || Session::has('error-message') || Session::has('message'))
     <div class="container">
         @if(Session::has('success-message'))
-            <div class="notice-success" data-dismiss="timeout">
+            <div class="notice notice-success" onClick="$(this).remove();" data-dismiss="timeout">
                 {{ Session::get('success-message') }}
             </div>
         @endif
         @if(Session::has('error-message'))
-            <div class="notice-error" data-dismiss="timeout">
+            <div class="notice notice-error" onClick="$(this).remove();" data-dismiss="timeout">
                 {{ Session::get('error-message') }}
             </div>
         @endif
         @if(Session::has('message'))
-            <div class="notice-info" data-dismiss="timeout">
+            <div class="notice notice-info" onClick="$(this).remove();" data-dismiss="timeout">
                 {{ Session::get('message') }}
             </div>
         @endif
