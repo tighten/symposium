1f9063e1ae2cef200806f073531b5957f7d18941
Michael Jones <michaelrtm@gmail.com>
2016-08-14 00:27:50 +1000

    vocab

--- database/migrations/2016_08_13_102611_add_profile_picture_to_users.php
+++ database/migrations/2016_08_13_102611_add_profile_picture_to_users.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddProfileImageToUsers extends Migration
+class AddProfilePictureToUsers extends Migration
 {
     /**
      * Run the migrations.
