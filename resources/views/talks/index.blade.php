a800ec3fe1dfef1660191fb5def1abb53f7f1c7f
Matt Stauffer <matt@tighten.co>
2016-08-17 13:23:32 -0400

    Wording

--- resources/views/talks/index.blade.php
+++ resources/views/talks/index.blade.php
@@ -9,7 +9,7 @@
                     <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
                 </a>
                 <h2 class="page-title">My Talks</h2>
-                <a href="{{ route('talks.archived.index') }}" class="btn btn-default btn-xs">My Archived Talks</a>
+                <a href="{{ route('talks.archived.index') }}" class="btn btn-default btn-xs">Show Archived Talks</a>
                 <p class="list-sort">Sort: {{ $sorted_by }}
                     <a href="{{ route('talks.index', ['sort' => 'alpha']) }}" class="@sorted($sorted_by, 'alpha')">Title</a> |
                     <a href="{{ route('talks.index', ['sort' => 'date']) }}" class="@sorted($sorted_by, 'date')">Date</a>
