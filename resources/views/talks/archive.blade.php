9cdd10aef904a4b30228f1b2d0a4058175859b88
Matt Stauffer <matt@tighten.co>
2016-08-17 13:21:25 -0400

    Clarify button nav for archive/active talks

--- resources/views/talks/archive.blade.php
+++ resources/views/talks/archive.blade.php
@@ -4,8 +4,8 @@
     <div class="container body">
         <div class="row">
             <div class="col-md-8 col-md-push-2">
-                <h2 class="page-title">Archive</h2>
-                <a href="/talks" class="btn btn-default btn-xs">Back</a>
+                <h2 class="page-title">My Archived Talks</h2>
+                <a href="/talks" class="btn btn-default btn-xs">Show Active Talks</a>
                 <p class="list-sort">Sort:
                     <a href="{{ route('talks.archived.index', ['sort' => 'alpha']) }}" class="@sorted($sorted_by, 'alpha')">Title</a> |
                     <a href="{{ route('talks.archived.index', ['sort' => 'date']) }}" class="@sorted($sorted_by, 'date')">Date</a>
