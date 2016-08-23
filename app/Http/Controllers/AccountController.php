853663b4f821b38c7892df4c2c8c00ea5bef1de6
Michael Jones <michaelrtm@gmail.com>
2016-08-24 01:00:38 +1000

    Had changes from another PR because I'm noob @ git

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -62,11 +62,10 @@ class AccountController extends BaseController
             'enable_profile' => '',
             'allow_profile_contact' => '',
             'profile_intro' => '',
-            'profile_slug' => 'alpha_dash|required_if:enable_profile,1|unique:users,profile_slug,' . Auth::user()->id,
+            'profile_slug' => 'alpha_dash|unique:users,profile_slug,' . Auth::user()->id,
             'profile_picture' => 'image|max:5000',
         ], [
             'profile_picture.max' => 'Profile picture cannot be larger than 5mb',
-            'profile_slug.required_if' => 'You must set a Profile URL Slug to enable your Public Speaker Profile'
         ]);
 
         // Save
