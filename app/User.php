tighten/symposium

commit aa59503a058746dc2888885a658799323a001a70

Global sort user talks alphabetically.
Closes #97.

app/User.php
@@ -20,11 +20,13 @@ class User extends Authenticatable
     public function talks()
     {
         return $this->hasMany('Talk', 'author_id');
-        /* @todo can we do this somehow?
-            ->orderBy(function ($talk) {
-                return strtolower($talk->current()->title);
-            });
-        */
+    }
+
+    public function getTalksAttribute()
+    {
+        return $this->talks()->get()->sortBy(function ($talk) {
+            return strtolower($talk->current()->title);
+        });
     }
 
     public function bios()

app/Http/Controllers/Api/UserTalksController.php
@@ -20,8 +20,6 @@ class UserTalksController extends BaseController
 
         $return = OAuthGuard::user()->talks->map(function ($talk) {
             return new Talk($talk);
-        })->sortBy(function ($talk) {
-            return $talk->attributes()['title']; 
         })->values();
 
         return response()->jsonApi([

tests/TalkTest.php
@@ -20,4 +20,30 @@ class TalkTest extends IntegrationTestCase
              ->visit('talks/' . $talk->id)
              ->see($revision->title);
     }
+
+    /** @test */
+    public function talks_are_sorted_alphabetically()
+    {
+        $user = Factory::create('user');
+        $talk1 = Factory::create('talk', [
+            'author_id' => $user->id
+        ]);
+        $revision1 = Factory::create('talkRevision', [
+            'title' => 'zyxwv'
+        ]);
+        $talk1->revisions()->save($revision1);
+
+        $talk2 = Factory::create('talk', [
+            'author_id' => $user->id
+        ]);
+        $revision2 = Factory::create('talkRevision', [
+            'title' => 'abcde'
+        ]);
+        $talk2->revisions()->save($revision2);
+
+        $talks = $user->talks;
+
+        $this->assertEquals('abcde', $talks->first()->current()->title);
+        $this->assertEquals('zyxwv', $talks->last()->current()->title);
+    }
 }

commit 3fc8d42acee7a3a7744b37905af241f385ee1580

Bugfix: Bug introduced in aa59503a returning associative array
Laravel's Illuminate\Support\Collection::sortBy() preserves the keys,
and therefore will always return an associative array, even when it
used to be sequential. If you json_encode the result, you might
expect an array, but get an object instead. This commit fixes that
issue, by discarding the array keys, and fetching only its values.
This fixes an issue with vue.js not being able to .forEach over an
object.

app/User.php
@@ -30,7 +30,7 @@ class User extends Authenticatable
     {
         return $this->talks()->get()->sortBy(function ($talk) {
             return strtolower($talk->current()->title);
-        });
+        })->values();
     }
 
     public function bios()