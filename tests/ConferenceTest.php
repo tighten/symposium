24c3548ba7a6360b26e5c712ecd8d8e1fc9523aa
Matt Stauffer <matt@tighten.co>
2016-11-06 13:53:03 -0500

    Some public conference tests

--- tests/ConferenceTest.php
+++ tests/ConferenceTest.php
@@ -6,7 +6,7 @@ use Carbon\Carbon;
 class ConferenceTest extends IntegrationTestCase
 {
     /** @test */
-    public function conferences_accept_proposals_during_the_call_for_papers()
+    function conferences_accept_proposals_during_the_call_for_papers()
     {
         $conference = Factory::create('conference', [
             'cfp_starts_at' => Carbon::yesterday(),
@@ -17,7 +17,7 @@ class ConferenceTest extends IntegrationTestCase
     }
 
     /** @test */
-    public function conferences_dont_accept_proposals_outside_of_the_call_for_papers()
+    function conferences_dont_accept_proposals_outside_of_the_call_for_papers()
     {
         $conference = Factory::create('conference', [
             'cfp_starts_at' => Carbon::tomorrow(),
@@ -35,7 +35,7 @@ class ConferenceTest extends IntegrationTestCase
     }
 
     /** @test */
-    public function conferences_that_havent_announced_their_cfp_are_not_accepting_proposals()
+    function conferences_that_havent_announced_their_cfp_are_not_accepting_proposals()
     {
         $conference = Factory::create('conference', [
             'cfp_starts_at' => null,
@@ -46,16 +46,48 @@ class ConferenceTest extends IntegrationTestCase
     }
 
     /** @test */
-    public function non_owners_can_view_conference()
+    function non_owners_can_view_conference()
     {
-        $user = Factory::create('user');        
+        $user = Factory::create('user');
 
-        $otherUser = Factory::create('user');        
+        $otherUser = Factory::create('user');
         $conference = Factory::build('conference');
         $otherUser->conferences()->save($conference);
 
         $this->actingAs($user)
-             ->visit('conferences/' . $conference->id)
-             ->see($conference->title);
+            ->visit('conferences/' . $conference->id)
+            ->see($conference->title);
+    }
+
+    /** @test */
+    function guests_can_view_conference()
+    {
+        $user = Factory::create('user');
+
+        $conference = Factory::build('conference');
+        $user->conferences()->save($conference);
+
+        $this->visit('conferences/' . $conference->id)
+            ->see($conference->title);
+    }
+
+    /** @test */
+    function guests_can_view_conference_list()
+    {
+        $user = Factory::create('user');
+
+        $conference = Factory::build('conference');
+        $user->conferences()->save($conference);
+
+        $this->visit('conferences?filter=all')
+            ->seePageIs('conferences?filter=all')
+            ->see($conference->title);
+    }
+
+    /** @test */
+    function guests_cannot_create_conference()
+    {
+        $this->visit('conferences/create')
+            ->seePageIs('log-in');
     }
 }
