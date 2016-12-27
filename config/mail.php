5235ed80ac9a9712e86d2b3db2c9d3aec1e02393
Matt Stauffer <matt@tighten.co>
2016-12-27 07:29:09 -0500

    Add mail config

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
 	|
 	*/
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'noreply@symposiumapp.com', 'name' => 'Symposium'],
 
     /*
 	|--------------------------------------------------------------------------
