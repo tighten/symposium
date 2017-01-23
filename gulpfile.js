dd534bc1b13f7615feba129e7ff98265f1bb5343
Matt Stauffer <matt@tighten.co>
2017-01-23 09:21:17 -0500

    Fix gulp browserify error

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,6 @@ const elixir = require('laravel-elixir');
 require('laravel-elixir-vueify');
 
 elixir((mix) => {
-    mix.sass('app.scss')
-        browserify('app.js');
+    mix.sass('app.scss');
+    mix.browserify('app.js');
 });
