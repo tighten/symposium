c7e031a71de7d3fc95e1242455524579b8986682
Jose Soto <jose@homehero.org>
2017-02-22 13:11:30 -0800

    Moves location script into resources and processes it via Elixir

--- resources/views/account/edit.blade.php
+++ resources/views/account/edit.blade.php
@@ -112,51 +112,6 @@
 @stop
 
 @push('scripts')
-<script type="text/javascript">
-var autocomplete;
-var componentForm = {
-    neighborhood: 'long_name', // neighborhood within borough (Williamsburg)
-    sublocality_level_1: 'long_name', // borough within city (Brooklyn)
-    locality: 'long_name', // city (New York, Los Angeles)
-    administrative_area_level_1: 'short_name', // state (NY, CA)
-    country: 'short_name', // country (US)
-};
-
-function initAutocomplete() {
-    var input = document.getElementById('autocomplete');
-
-    autocomplete = new google.maps.places.Autocomplete(input);
-    autocomplete.addListener('place_changed', fillPlaceData);
-
-    // Prevent submitting account form when selecting location
-    input.addEventListener("keypress", function(event){
-        if (event.which == '13') {
-            event.preventDefault();
-        }
-    });
-}
-
-function fillPlaceData() {
-    var place = autocomplete.getPlace();
-    clearComponents();
-    if(place.address_components) {
-        place.address_components.forEach(setComponentInput);
-    }
-}
-
-function clearComponents() {
-    for (var component in componentForm) {
-        document.getElementById(component).value = '';
-    }
-}
-
-function setComponentInput(component) {
-    var addressType = component.types[0];
-    if (componentForm[addressType]) {
-        var val = component[componentForm[addressType]];
-        document.getElementById(addressType).value = val;
-    }
-}
-</script>
+<script src="{{ elixir('js/location.js') }}"></script>
 <script src="https://maps.googleapis.com/maps/api/js?key={{ config('services.google.maps.key') }}&libraries=places&callback=initAutocomplete" async defer></script>
 @endpush
