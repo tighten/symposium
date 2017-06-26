9d18fc703c451b2c05f7fb03d06b67c3b20252f6
Matt Stauffer <matt@tighten.co>
2017-06-26 10:58:03 -0400

    Exclude auth routes from CSRF

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -6,12 +6,7 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
 
 class VerifyCsrfToken extends BaseVerifier
 {
-    /**
-     * The URIs that should be excluded from CSRF verification.
-     *
-     * @var array
-     */
     protected $except = [
-        //
+        'oauth/*',
     ];
 }
