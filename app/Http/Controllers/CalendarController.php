2b601f01c6068f1295c46c14925bd46b9cc7815c
John T. Bonaccorsi <jtbonaccorsi@gmail.com>
2017-07-12 16:28:17 -0400

    Split CFP’s into 2 events

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -10,7 +10,8 @@ class CalendarController extends BaseController
     public function index()
     {
         $calendar = Calendar::addEvents($this->conferenceEvents())
-            ->addEvents($this->cfpEvents());
+            ->addEvents($this->cfpStartEvents())
+            ->addEvents($this->cfpEndEvents());
 
         return view('calendar', [
             'calendar' => $calendar,
@@ -35,17 +36,34 @@ class CalendarController extends BaseController
         })->toArray();
     }
 
-    private function cfpEvents()
+    private function cfpStartEvents()
     {
         return Conference::all()->map(function ($conference) {
             return Calendar::event(
                 "CFPs open for {$conference->title}",
                 true,
                 $conference->cfp_starts_at,
-                $conference->cfp_ends_at,
+                $conference->cfp_starts_at,
                 'cfp-' . $conference->id,
                 [
                     'color' => '#F39C12',
+                    'url' => route('conferences.show', $conference->id),
+                ]
+            );
+        })->toArray();
+    }
+
+    private function cfpEndEvents()
+    {
+        return Conference::all()->map(function ($conference) {
+            return Calendar::event(
+                "CFPs close for {$conference->title}",
+                true,
+                $conference->cfp_ends_at,
+                $conference->cfp_ends_at,
+                'cfp-' . $conference->id,
+                [
+                    'color' => '#E74C3C',
                     'url' => route('conferences.show', $conference->id),
                 ]
             );
