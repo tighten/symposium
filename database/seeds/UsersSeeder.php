cb6e6c3df23a55ab781767db95f7255d60b442dc
Tom Diggle <tom@tomdiggle.com>
2017-11-30 18:09:39 +0000

    Change to use new factories

--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -1,28 +1,19 @@
 <?php
 
 use App\User;
-use Faker\Factory as Faker;
 use Illuminate\Database\Seeder;
-use Laracasts\TestDummy\Factory;
 
 class UsersSeeder extends Seeder
 {
     public function run()
     {
         User::truncate();
-        $faker = Faker::create();
 
-        User::create([
-            'email' => 'matt@savemyproposals.com',
-            'password' => Hash::make('password'),
+        factory(App\User::class)->create([
             'name' => 'Matt Stauffer',
+            'email' => 'matt@savemyproposals.com',
         ]);
 
-        foreach (range(1, 10) as $i) {
-            Factory::create('user', [
-                'email' => $faker->email,
-                'name' => $faker->name
-            ]);
-        }
+        factory(App\User::class)->create([], 10);
     }
 }
