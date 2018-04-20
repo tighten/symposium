b9bf209fb8983f580422da857e41ee8c00745cde
Jose Soto <josecanhelp@gmail.com>
2018-04-20 12:14:56 -0400

    Social Login Redirect to Dashboard

--- app/Http/Controllers/Auth/SocialLoginController.php
+++ app/Http/Controllers/Auth/SocialLoginController.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Controllers\Auth;
 
-use Auth;
 use App\Http\Controllers\Controller;
-use App\Http\Requests;
 use App\User;
+use Auth;
 use Illuminate\Http\Request;
 use Socialite;
 
@@ -15,32 +14,32 @@ class SocialLoginController extends Controller
     {
         return Socialite::driver($service)->redirect();
     }
-
+    
     public function callback($service, Request $request)
     {
         $serviceUser = Socialite::driver($service)->user();
-
+        
         $user = $this->getExistingUser($serviceUser, $service);
-
+        
         if (! $user) {
             $user = User::create([
                 'name' => $serviceUser->getName(),
                 'email' => $serviceUser->getEmail(),
             ]);
         }
-
+        
         if (! $user->hasSocialLinked($service)) {
             $user->social()->create([
                 'social_id' => $serviceUser->getId(),
                 'service' => $service,
             ]);
         }
-
+        
         Auth::login($user, false);
-
-        return redirect()->intended();
+        
+        return redirect()->route('dashboard');
     }
-
+    
     protected function getExistingUser($serviceUser, $service)
     {
         return User::where('email', $serviceUser->getEmail())->orWhereHas('social', function ($q) use ($serviceUser, $service) {
