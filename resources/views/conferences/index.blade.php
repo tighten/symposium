1e4fa399e270c9d10ab60c17c61233c728874a04
Andrew Morgan <andrewmile@gmail.com>
2019-03-05 10:00:40 -0500

    Move favorite action above dismiss action

--- resources/views/conferences/index.blade.php
+++ resources/views/conferences/index.blade.php
@@ -42,19 +42,19 @@
                     @forelse ($conferences as $conference)
                         <li>
                             <div class="conference-actions">
-                                @if (Auth::check() && !$conference->isFavorited())
-                                    @if ($conference->isDismissed())
-                                        <a href="/conferences/{{ $conference->id  }}/undismiss" class="action-button" title="I am interested in this conference"><span class="glyphicon glyphicon-plus"></a>
-                                    @else
-                                        <a href="/conferences/{{ $conference->id  }}/dismiss" class="action-button" title="I am not interested in this conference"><span class="glyphicon glyphicon-remove"></a>
-                                    @endif
-                                @endif
-
                                 @if (Auth::check() && !$conference->isDismissed())
                                     @if ($conference->isFavorited())
                                         <a href="/conferences/{{ $conference->id  }}/unfavorite" class="action-button action-button--faved"><span class="glyphicon glyphicon-star"></a>
                                     @else
                                         <a href="/conferences/{{ $conference->id  }}/favorite" class="action-button"><span class="glyphicon glyphicon-star-empty"></a>
+                                    @endif
+                                @endif
+
+                                @if (Auth::check() && !$conference->isFavorited())
+                                    @if ($conference->isDismissed())
+                                        <a href="/conferences/{{ $conference->id  }}/undismiss" class="action-button" title="I am interested in this conference"><span class="glyphicon glyphicon-plus"></a>
+                                    @else
+                                        <a href="/conferences/{{ $conference->id  }}/dismiss" class="action-button" title="I am not interested in this conference"><span class="glyphicon glyphicon-remove"></a>
                                     @endif
                                 @endif
                             </div>
