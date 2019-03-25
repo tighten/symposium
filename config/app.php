f94393f0435b8b74b5d7540e187bb29f957ec290
Laravel Shift <shift@laravelshift.com>
2019-03-25 14:15:40 +0000

    Shift configuration

--- config/app.php
+++ config/app.php
@@ -154,7 +154,6 @@ return [
         'Illuminate\Bus\BusServiceProvider',
         Laravel\Scout\ScoutServiceProvider::class,
         Laravel\Passport\PassportServiceProvider::class,
-        Laravel\Tinker\TinkerServiceProvider::class,
 
         /*
          * Application Service Providers...
