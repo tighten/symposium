a6ef1842989e56fd4aece54a53bcc476f5264af4
Tom Diggle <tom@tomdiggle.com>
2019-09-03 21:45:12 +0100

    Change to extend AuthenticateUsers login method

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -19,7 +19,9 @@ class LoginController extends Controller
     |
     */
 
-    use AuthenticatesUsers;
+    use AuthenticatesUsers {
+        login as loginUser;
+    }
 
     /**
      * Where to redirect users after login.
@@ -48,26 +50,6 @@ class LoginController extends Controller
      */
     public function login(LoginFormRequest $request)
     {
-        $this->validateLogin($request);
-
-        // If the class is using the ThrottlesLogins trait, we can automatically throttle
-        // the login attempts for this application. We'll key this by the username and
-        // the IP address of the client making these requests into this application.
-        if (method_exists($this, 'hasTooManyLoginAttempts') &&
-            $this->hasTooManyLoginAttempts($request)) {
-            $this->fireLockoutEvent($request);
-            return $this->sendLockoutResponse($request);
-        }
-
-        if ($this->attemptLogin($request)) {
-            return $this->sendLoginResponse($request);
-        }
-
-        // If the login attempt was unsuccessful we will increment the number of attempts
-        // to login and redirect the user back to the login form. Of course, when this
-        // user surpasses their maximum number of attempts they will get locked out.
-        $this->incrementLoginAttempts($request);
-        
-        return $this->sendFailedLoginResponse($request);
+        return $this->loginUser($request);
     }
 }
