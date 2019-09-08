c9d0e8ab13efabd48f1e37fc382912f1470c06da
Tom Diggle <tom@tomdiggle.com>
2019-09-08 10:48:34 +0100

    Refactor github login button
    The Github login button has been refactored to a tailwind component so it can be easily reused.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,29 @@ module.exports = {
     extend: {}
   },
   variants: {},
-  plugins: []
+  plugins: [
+    function({ addComponents }) {
+      const buttons = {
+        '.btn-github-login': {
+          margin: '.25rem 0',
+          padding: '.5rem',
+          display: 'inline-block',
+          borderRadius: '.25rem',
+          borderWidth: '1px',
+          borderColor: '#2d3748',
+          textDecoration: 'none',
+          color: '#2d3748',
+          fontSize: '1.125rem',
+          '&:hover': {
+            backgroundColor: '#4299e1',
+            borderColor: '#4299e1',
+            color: '#ffffff',
+            textDecoration: 'none'
+          },
+        },
+      }
+
+      addComponents(buttons)
+    }
+  ]
 }
