9ad8842bec275dfd3360a30fa5e81dbd014cf97b
Andrew Morgan <andrewmile@gmail.com>
2020-01-13 16:06:42 -0600

    Add responsive styles to header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,17 +1,17 @@
 @include('partials.flash-messages')
-<main-header class="sm:flex sm:justify-between sm:items-center sm:px-4 sm:py-3 px-8 py-12 max-w-md mx-auto sm:max-w-6xl">
+<main-header class="sm:flex sm:justify-between sm:items-center sm:py-3 px-4 py-12 max-w-md mx-auto sm:max-w-6xl">
     <template slot-scope="slotProps">
-        <div class="flex items-center justify-between px-4 py-3 sm:p-0">
-            <div>
-                <img class="h-10" src="/img/symposium-logo.svg" alt="Symposium">
-            </div>
-            <div class="sm:hidden">
-                <button v-on:click="slotProps.toggleNav" type="button" class="block text-indigo hover:text-white focus:text-white focus:outline-none">
-                    <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
-                        <path v-if="slotProps.showNav" fill-rule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/>
-                        <path v-else fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
-                    </svg>
-                </button>
+        <div class="px-4 py-3 sm:p-0">
+            <div class="flex items-center justify-between">
+                <img class="h-10 sm:h-8 md:h-10" src="/img/symposium-logo.svg" alt="Symposium">
+                <div class="sm:hidden">
+                    <button v-on:click="slotProps.toggleNav" type="button" class="block text-indigo hover:text-black focus:outline-none">
+                        <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+                            <path v-if="slotProps.showNav" fill-rule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/>
+                            <path v-else fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
+                        </svg>
+                    </button>
+                </div>
             </div>
         </div>
         @include('partials.nav')
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,30 +1,80 @@
-<nav>
-    <div :class="slotProps.showNav ? 'block' : 'hidden'" class="px-2 pt-2 pb-4 sm:flex font-sans font-semibold text-sm items-center">
-        @if (Auth::check())
-            <a class="block px-2 sm:px-0 py-1 sm:mr-6 text-indigo no-underline font-semibold rounded hover-bg-indigo-800 uppercase" href="{{ route('dashboard') }}">Dashboard</a>
-            <a class="mt-1 sm:mt-0 ml-2 sm:mx-6 block px-2 sm:px-0 py-1 text-indigo no-underline font-semibold rounded hover-bg-indigo-800 uppercase" href="{{ route('bios.index') }}">Bios</a>
-            <a class="mt-1 sm:mt-0 ml-2 sm:mx-6 block px-2 sm:px-0 py-1 text-indigo no-underline font-semibold rounded hover-bg-indigo-800 uppercase" href="{{ route('conferences.index') }}">Conferences</a>
-            <a class="mt-1 sm:mt-0 ml-2 sm:mx-6 block px-2 sm:px-0 py-1 text-indigo no-underline font-semibold rounded hover-bg-indigo-800 uppercase" href="{{ route('calendar.index') }}">Calendar</a>
-            <a class="mt-1 sm:mt-0 ml-2 sm:ml-6 block px-2 sm:px-0 py-1 text-indigo no-underline font-semibold rounded hover-bg-indigo-800 uppercase" href="{{ route('talks.index') }}">Talks</a>
-            {{-- <li class="" role="presentation">
-                <a class="" data-toggle="dropdown" href="#" role="button" aria-expanded="false">
-                    <img src="{{ Auth::user()->profile_picture_thumb }}" class="nav-profile-picture inline"> Me <span class="caret"></span>
-                </a>
-                <ul class="dropdown-menu" role="menu">
-                    <a href="{{ route('account.show') }}">Account</a>
-                    @if (Auth::user()->enable_profile)
-                    <a href="{{ route('speakers-public.show', [Auth::user()->profile_slug]) }}">Public Speaker Profile</a>
-                    @endif
-                    <a href="{{ route('log-out') }}">Log out</a>
-                </ul> --}}
+<nav :class="slotProps.showNav ? 'block' : 'hidden'" class="pt-2 py-2 pl-4 sm:pl-0 sm:flex font-sans font-semibold text-sm text-indigo items-center block">
+    @if (Auth::check())
+        @php
+            $menuItems = [
+                [
+                    'title' => 'Dashboard',
+                    'url' => route('dashboard'),
+                    'classes' => 'mr-4 sm:mr-2 md:mr-4',
+                ],
+                [
+                    'title' => 'Bios',
+                    'url' => route('bios.index'),
+                    'classes' => 'mr-4 sm:mx-2 md:mx-4 mt-2 sm:mt-0',
+                ],
+                [
+                    'title' => 'Conferences',
+                    'url' => route('conferences.index'),
+                    'classes' => 'mr-4 sm:mx-2 md:mx-4 mt-2 sm:mt-0',
+                ],
+                [
+                    'title' => 'Calendar',
+                    'url' => route('calendar.index'),
+                    'classes' => 'mr-4 sm:mx-2 md:mx-4 mt-2 sm:mt-0',
+                ],
+                [
+                    'title' => 'Talks',
+                    'url' => route('talks.index'),
+                    'classes' => 'mr-4 sm:mx-2 md:mx-4 mt-2 sm:mt-0 border-none',
+                ],
 
-        @else
-            <a class="sm:mt-0 block px-2 sm:px-0 py-1 sm:mr-6 text-indigo no-underline font-semibold rounded hover-bg-indigo-800 uppercase" href="{{ url('what-is-this') }}">How it Works</a>
-            <a class="mt-1 sm:mt-0 ml-2 sm:mx-6 block px-2 sm:px-0 py-1 text-indigo no-underline font-semibold rounded hover-bg-indigo-800 uppercase" href="{{ url('speakers') }}">Our speakers</a>
-            <a class="mt-1 sm:mt-0 ml-2 sm:mx-6 block px-2 sm:px-0 py-1 text-indigo no-underline font-semibold rounded hover-bg-indigo-800 uppercase" href="{{ route('conferences.index') }}">Conferences</a>
-            <a class="mt-1 sm:mt-0 ml-2 sm:ml-6 block px-4 py-2 text-indigo no-underline font-semibold rounded-lg hover-bg-indigo-800 border-indigo border rounded" href="{{ route('login') }}">Sign in</a>
-            {{-- Disable email registration --}}
-            {{-- <a href="{{ route('register') }}">Sign up</a> --}}
-        @endif
-    </div>
+            ];
+        @endphp
+        {{-- <li class="" role="presentation">
+            <a class="" data-toggle="dropdown" href="#" role="button" aria-expanded="false">
+                <img src="{{ Auth::user()->profile_picture_thumb }}" class="nav-profile-picture inline"> Me <span class="caret"></span>
+            </a>
+            <ul class="dropdown-menu" role="menu">
+                <a href="{{ route('account.show') }}">Account</a>
+                @if (Auth::user()->enable_profile)
+                <a href="{{ route('speakers-public.show', [Auth::user()->profile_slug]) }}">Public Speaker Profile</a>
+                @endif
+                <a href="{{ route('log-out') }}">Log out</a>
+            </ul> --}}
+
+    @else
+        @php
+            $menuItems = [
+                [
+                    'title' => 'How it Works',
+                    'url' => url('what-is-this'),
+                    'classes' => 'mr-4 sm:mr-2 md:mr-4',
+                ],
+                [
+                    'title' => 'Our speakers',
+                    'url' => url('speakers'),
+                    'classes' => 'mr-4 sm:mx-2 md:mx-4 mt-2 sm:mt-0',
+                ],
+                [
+                    'title' => 'Conferences',
+                    'url' => route('conferences.index'),
+                    'classes' => 'mr-4 sm:mx-2 md:mx-4 mt-2 sm:mt-0',
+                ],
+            ];
+        @endphp
+    @endif
+
+    @foreach ($menuItems as $item)
+        <a class="block border-b border-indigo border-solid hover-bg-indigo-800 py-1 sm:border-none uppercase {{ $item['classes'] }}"
+           href="{{ $item['url'] }}"
+        >
+            {{ $item['title'] }}
+        </a>
+    @endforeach
+
+    @if (! Auth::check())
+        <a class="border border-indigo hover-bg-indigo-800 inline-block md:ml-4 mt-4 px-8 py-2 rounded rounded-lg sm:block sm:ml-2 sm:mt-0 sm:mt-1 sm:px-4" href="{{ route('login') }}">Sign in</a>
+    @endif
+    {{-- Disable email registration --}}
+    {{-- <a href="{{ route('register') }}">Sign up</a> --}}
 </nav>
