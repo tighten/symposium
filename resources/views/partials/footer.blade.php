b4b7cd638bbef237a24ccff30319e8244e353c2e
Andrew Morgan <andrewmile@gmail.com>
2020-01-15 14:40:55 -0600

    Add responsive styles to footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,11 +1,11 @@
 <div class="bg-black pb-12">
     <div class="font-sans text-lg text-gray-500 max-w-md mx-auto sm:max-w-6xl">
         <footer>
-            <div class="flex justify-between">
+            <div class="flex flex-col lg:flex-row justify-between px-8 xl:px-0 text-center lg:text-left">
                 <div>&copy; <a href="http://tighten.co/">Tighten Co.</a> {{ date('Y') }}</div>
-                <div>|</div>
+                <div class="invisible lg:visible">|</div>
                 <div>Source &amp; roadmap on <a href="https://github.com/tightenco/symposium">GitHub</a></div>
-                <div>|</div>
+                <div class="invisible lg:visible">|</div>
                 <div>Submit your talks easily with <a href="http://rdohms.github.io/pronto/">Pronto!</a></div>
             </div>
         </footer>
