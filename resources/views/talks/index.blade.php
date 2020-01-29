d0589ec2ea0a4135b34d1cbcc396ec5418452cbb
Andrew Morgan <andrewmile@gmail.com>
2020-01-29 09:28:40 -0600

    Fix indenting on talks.index view

--- resources/views/talks/index.blade.php
+++ resources/views/talks/index.blade.php
@@ -5,33 +5,32 @@
     <div class="body">
         <div class="row">
             <div class="flex py-3 max-w-md mx-auto sm:max-w-3xl">
-            <div class="w-1/4">
-                @php
-                    $linkRouteKeysWithDefaults = ['filter' => 'active', 'sort' => 'alpha'];
-                    $inactiveLinkClasses = 'filter-link p-1 rounded';
-                @endphp
-
-                <div class="border-2 border-indigo-200 rounded mt-4 font-sans">
-                    <div class="bg-indigo-150 p-4">Filter</div>
-                    <div class="bg-white flex flex-col p-4">
-                        {!! HTML::activeLinkRoute($linkRouteKeysWithDefaults, 'talks.index', 'Active', ['filter' => 'active'], ['class' => $inactiveLinkClasses]) !!}
-                        <a href="{{ route('talks.archived.index') }}" class="{{ $inactiveLinkClasses }}">Archived</a>
+                <div class="w-1/4">
+                    @php
+                        $linkRouteKeysWithDefaults = ['filter' => 'active', 'sort' => 'alpha'];
+                        $inactiveLinkClasses = 'filter-link p-1 rounded';
+                    @endphp
+                    <div class="border-2 border-indigo-200 rounded mt-4 font-sans">
+                        <div class="bg-indigo-150 p-4">Filter</div>
+                        <div class="bg-white flex flex-col p-4">
+                            {!! HTML::activeLinkRoute($linkRouteKeysWithDefaults, 'talks.index', 'Active', ['filter' => 'active'], ['class' => $inactiveLinkClasses]) !!}
+                            <a href="{{ route('talks.archived.index') }}" class="{{ $inactiveLinkClasses }}">Archived</a>
+                        </div>
                     </div>
-                </div>
 
-                <div class="border-2 border-indigo-200 bg-gray-200 rounded mt-4">
-                    <div class="bg-indigo-150 p-4">Sort</div>
-                    <div class="bg-white flex flex-col p-4">
-                        {!! HTML::activeLinkRoute($linkRouteKeysWithDefaults, 'talks.index', 'Title', ['sort' => 'alpha'], ['class' => $inactiveLinkClasses]) !!}
-                        {!! HTML::activeLinkRoute($linkRouteKeysWithDefaults, 'talks.index', 'Date', ['sort' => 'date'], ['class' => $inactiveLinkClasses]) !!}
+                    <div class="border-2 border-indigo-200 bg-gray-200 rounded mt-4">
+                        <div class="bg-indigo-150 p-4">Sort</div>
+                        <div class="bg-white flex flex-col p-4">
+                            {!! HTML::activeLinkRoute($linkRouteKeysWithDefaults, 'talks.index', 'Title', ['sort' => 'alpha'], ['class' => $inactiveLinkClasses]) !!}
+                            {!! HTML::activeLinkRoute($linkRouteKeysWithDefaults, 'talks.index', 'Date', ['sort' => 'date'], ['class' => $inactiveLinkClasses]) !!}
+                        </div>
                     </div>
+                    <a href="{{ route('talks.create') }}"
+                       class="mt-4 w-full bg-indigo-500 text-white rounded px-4 py-2 block text-center">Add Talk &nbsp;<span
+                            class="glyphicon glyphicon-plus"
+                            aria-hidden="true"></span>
+                    </a>
                 </div>
-                <a href="{{ route('talks.create') }}"
-                   class="mt-4 w-full bg-indigo-500 text-white rounded px-4 py-2 block text-center">Add Talk &nbsp;<span
-                        class="glyphicon glyphicon-plus"
-                        aria-hidden="true"></span>
-                </a>
-            </div>
                 <div class="w-3/4 ml-4">
                     @foreach ($talks as $talk)
                         <div class="border-2 border-indigo-200 rounded mt-4 hover:border-indigo">
