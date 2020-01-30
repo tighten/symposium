211da5bdcbbaf170513ac6ce3944d1f8e12c0ac0
Andrew Morgan <andrewmile@gmail.com>
2020-01-30 10:52:35 -0600

    Add style for empty state on conferences page

--- resources/views/conferences/index.blade.php
+++ resources/views/conferences/index.blade.php
@@ -101,7 +101,9 @@
                             </div>
                         </div>
                     @empty
-                        No conferences match this filter
+                        <div class="border-2 border-indigo-200 rounded mt-4 p-4">
+                            No conferences match this filter
+                        </div>
                     @endforelse
                 </div>
             </div>
