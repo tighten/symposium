f2f23dfa474649e76b79084848bdb5504a33d874
Andrew Morgan <andrewmile@gmail.com>
2020-02-03 11:52:30 -0600

    Redesign show bio page

--- resources/views/bios/show.blade.php
+++ resources/views/bios/show.blade.php
@@ -1,21 +1,35 @@
 @extends('layout')
 
 @section('content')
-    <div class="container body">
-        <div class="row">
-            <div class="col-md-6 col-md-push-3">
-                <p class="pull-right">
-                    <a href="{{ route('bios.edit', ['id' => $bio->id]) }}" class="btn btn-default">Edit &nbsp;<span class="glyphicon glyphicon-edit" aria-hidden="true"></span></a>
-                    <a href="{{ route('bios.delete', ['id' => $bio->id]) }}" class="btn btn-danger">Delete &nbsp;<span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a>
-                </p>
 
-                <h1>{{ $bio->nickname }}</h1>
-
-                <p class="bio-meta">Created {{ $bio->created_at->toFormattedDateString() }} | Updated {{ $bio->updated_at->toFormattedDateString() }}</p>
-
-                <p><!-- TODO: Figure out how we will be handling HTML/etc. -->
-                    {!! str_replace("\n", "<br>", $bio->body) !!}</p>
+<div class="max-w-md mx-auto sm:max-w-3xl border-2 border-indigo-200 rounded mt-4">
+    <div class="bg-white p-10">
+        <div class="flex items-center justify-between">
+            <h2 class="m-0 font-sans text-2xl">{{ $bio->nickname }}</h2>
+            <div class="text-indigo-500 text-lg">
+                <a href="{{ route('bios.edit', ['id' => $bio->id]) }}" title="Edit">
+                  <span class="glyphicon glyphicon-edit" aria-hidden="true"></span>
+                </a>
+                <a href="{{ route('bios.delete', ['id' => $bio->id]) }}" class="ml-3" title="Delete">
+                    <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+                </a>
             </div>
         </div>
+        <div class="mt-3 font-sans">
+            <!-- TODO: Figure out how we will be handling HTML/etc. -->
+            {!! str_replace("\n", "<br>", $bio->body) !!}
+        </div>
     </div>
+    <div class="bg-indigo-150 px-10 py-3 font-sans flex justify-between">
+        <div>
+            <div class="text-gray-500">Created</div>
+            <div>{{ $bio->created_at->toFormattedDateString() }}</div>
+        </div>
+        <div>
+            <div class="text-gray-500">Updated</div>
+            <div>{{ $bio->updated_at->toFormattedDateString() }}</div>
+       </div>
+    </div>
+</div>
+
 @stop
