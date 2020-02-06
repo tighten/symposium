e9e7ef748c78b81934699a692604ff70b568bab8
Andrew Morgan <andrewmile@gmail.com>
2020-02-06 17:06:38 -0600

    Move talks revision list into side panel

--- resources/views/talks/show.blade.php
+++ resources/views/talks/show.blade.php
@@ -1,32 +1,37 @@
 @extends('layout')
 
+@php
+    $baseLinkClasses = 'filter-link p-1 rounded';
+    $activeLinkClasses = 'font-bold text-indigo-500'
+@endphp
+
 @section('content')
 
-<div class="max-w-md mx-auto sm:max-w-3xl border-2 border-indigo-200 rounded mt-4">
-    <div class="bg-white p-10">
+<div class="flex flex-col md:flex-row py-3 max-w-md mx-auto sm:max-w-3xl mt-4">
+    <div class="w-full md:w-1/4">
+        <div class="w-1/2 md:w-full border-2 border-indigo-200 bg-white rounded font-sans">
+            <div class="bg-indigo-150 p-4">Revisions</div>
+            <div class="flex flex-col p-4">
+                @foreach ($talk->revisions as $revision)
+                    @if ($talk->current()->id == $revision->id)
+                        <a href="/talks/{{ $talk->id }}" class="{{ $baseLinkClasses }} {{ $revision->id == $current->id ? $activeLinkClasses : '' }}">{{ $revision->created_at }} <i>(current)</i></a>
+                    @else
+                        <a href="/talks/{{ $talk->id }}?revision={{ $revision->id }}" class="{{ $baseLinkClasses }} {{ $revision->id == $current->id ? $activeLinkClasses : '' }}">{{ $revision->created_at }}</a>
+                    @endif
+                @endforeach
+            </div>
+        </div>
+    </div>
+    <div class="w-full md:w-3/4 md:ml-4 bg-white p-10 border-2 border-indigo-200 rounded">
         <div class="flex items-center justify-between">
-            @if ($current === null)
-                <p>No revisions yet. @todo Throw exception etc.</p>
-            @else
-                <div>
-                    <h2 class="m-0 font-sans text-2xl">{{ $current->title }}</h2>
-                    <p style="font-style: italic;">
-                        {{ $current->length }} minute {{ $current->level }} {{ $current->type }}
-                    </p>
-                </div>
-            @endif
+            <div>
+                <h2 class="m-0 font-sans text-2xl">{{ $current->title }}</h2>
+                <p style="font-style: italic;">
+                    {{ $current->length }} minute {{ $current->level }} {{ $current->type }}
+                </p>
+            </div>
             <div class="text-indigo-500 text-lg">
-                @if ($showingRevision)
-                    <a href="/talks/{{ $talk->id }}" class="btn btn-default">Return to talk &nbsp;
-                        <span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span>
-                    </a>
-                    <br>
-                    <p style="text-align: right">REVISION:<br>
-                    {{ $current->created_at }}</p>
-                @else
-                    <a href="/talks/{{ $talk->id }}?revision={{ $talk->current()->id }}" title="Revisions">
-                      <span class="glyphicon glyphicon-th-list" aria-hidden="true"></span>
-                    </a>
+                @unless ($showingRevision)
                     <a href="/talks/{{ $talk->id }}/edit" class="ml-3" title="Edit">
                       <span class="glyphicon glyphicon-edit" aria-hidden="true"></span>
                     </a>
@@ -55,18 +60,6 @@
         @if ($current->slides)
             <h3 class="text-lg font-normal text-gray-500 mt-4">Slides</h3>
             <a href="{{ $current->slides }}">{{ $current->slides }}</a>
-        @endif
-
-        @if ($showingRevision)
-            <h3>Revisions</h3>
-            <ul>
-                @foreach ($talk->revisions as $revision)
-                    <li {{ $revision->id == $current->id ? 'style="font-weight: bold;"' : '' }}>
-                        <a href="/talks/{{ $talk->id }}?revision={{ $revision->id }}">{{ $revision->created_at }}</a>
-                        {!! $talk->current()->id == $revision->id ? '<i>(current)</i>' : '' !!}
-                    </li>
-                @endforeach
-            </ul>
         @endif
     </div>
 </div>
