8c6456d65ef7fdb70b55b170ca849ef4560f6dee
Andrew Morgan <andrewmile@gmail.com>
2020-02-11 17:22:37 -0600

    Add updated design to show account page

--- resources/views/account/show.blade.php
+++ resources/views/account/show.blade.php
@@ -1,43 +1,67 @@
-@extends('layout')
+@extends('layout', ['title' => 'Account'])
 
 @section('content')
 
-<div class="container body">
-    <div class="row">
-        <div class="col-md-8 col-md-push-2">
-            <h1>Account</h1>
-
-            <div class="actions-box">
-                <h3>Actions</h3>
-                <a href="{{ route('account.edit') }}">Edit account</a><br>
-                <a href="{{ route('account.delete') }}">Delete account</a><br>
-                <a href="{{ route('account.export') }}">Export data</a><br>
-                <a href="{{ route('account.oauth-settings') }}">oAuth settings</a><br>
+<div class="flex flex-col md:flex-row py-3 max-w-md mx-auto sm:max-w-3xl">
+    <div class="w-full md:w-1/4">
+        <div class="border-2 border-indigo-200 bg-white rounded mt-4 font-sans">
+            <div class="bg-indigo-150 p-4">Actions</div>
+            <div class="flex flex-col p-4">
+                <a href="{{ route('account.edit') }}" class="m-1">Edit account</a>
+                <a href="{{ route('account.delete') }}" class="m-1">Delete account</a>
+                <a href="{{ route('account.export') }}" class="m-1">Export data</a>
+                <a href="{{ route('account.oauth-settings') }}" class="m-1">oAuth settings</a>
+            </div>
+        </div>
+    </div>
+    <div class="w-full md:w-3/4 md:ml-4">
+        <div class="bg-white p-4 border-2 border-indigo-200 rounded mt-4">
+            <h3 class="font-sans m-0">User</h3>
+            <div class="mt-4">
+                <div class="text-gray-500">Email</div>
+                {{ $user->email }}
+            </div>
+            <div class="mt-4">
+                <div class="text-gray-500">Name</div>
+                {{ $user->name }}
+            </div>
+            <div class="mt-4">
+                <div class="text-gray-500">Speaker profile enabled?</div>
+                {{ $user->enable_profile ? 'Yes' : 'No' }}
+            </div>
+            <div class="mt-4">
+                <div class="text-gray-500">Speaker public contact allowed?</div>
+                {{ $user->allow_profile_contact ? 'Yes' : 'No' }}
             </div>
 
-            <h3>User</h3>
-            <p><b>Email:</b><br>{{ $user->email }}</p>
-            <p><b>Name:</b><br>{{ $user->name }}</p>
-            <p><b>Speaker profile enabled?</b><br>{{ $user->enable_profile ? 'Yes' : 'No' }}</p>
-            <p><b>Speaker public contact allowed?</b><br>{{ $user->allow_profile_contact ? 'Yes' : 'No' }}</p>
             @if ($user->profile_slug)
-                <p><b>Speaker profile URL slug:</b><br><a href="{{ route('speakers-public.show', [$user->profile_slug]) }}">{{ route('speakers-public.show', [$user->profile_slug]) }}</a></p>
+                <div class="mt-4">
+                    <div class="text-gray-500">Speaker profile URL slug</div>
+                    <a href="{{ route('speakers-public.show', [$user->profile_slug]) }}">{{ route('speakers-public.show', [$user->profile_slug]) }}</a>
+                </div>
             @endif
+
             @if ($user->location)
-                <p><b>Location:</b><br>{{ $user->location }}</p>
+                <div class="mt-4">
+                    <div class="text-gray-500">Location</div>
+                    {{ $user->location }}
+                </div>
             @endif
 
-            <p><b>Notifications enabled?</b><br>{{ $user->wants_notifications ? 'Yes' : 'No' }}</p>
-            <br><br>
-
-            <h4>Dismissed Conferences</h4>
-            <ul>
+            <div class="mt-4">
+                <div class="text-gray-500">Notifications enabled?</div>
+                {{ $user->wants_notifications ? 'Yes' : 'No' }}
+            </div>
+        </div>
+        <div class="bg-white p-4 border-2 border-indigo-200 rounded mt-4">
+            <h3 class="font-sans m-0">Dismissed Conferences</h3>
             @forelse ($user->dismissedConferences as $conference)
-                <li><a href="{{ route('conferences.show', ['id' => $conference->id]) }}">{{  $conference->title }}</a></li>
+                <div class="mt-4">
+                    <a href="{{ route('conferences.show', ['id' => $conference->id]) }}">{{  $conference->title }}</a>
+                </div>
             @empty
-                <li>(none)</li>
+                <div class="mt-4">(none)</div>
             @endforelse
-            </ul>
         </div>
     </div>
 </div>
