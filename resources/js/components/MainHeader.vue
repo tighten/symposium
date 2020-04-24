fc04fedca82d50ff89423766fadb11929b7918cc
Andrew Morgan <andrewmile@gmail.com>
2020-04-24 15:52:30 -0500

    Hide sign in menu on click away

--- resources/js/components/MainHeader.vue
+++ resources/js/components/MainHeader.vue
@@ -14,6 +14,18 @@
 <script>
     export default {
         name: 'MainHeader',
+        mounted() {
+            const listener = e => {
+                if (e.target === this.$el || this.$el.contains(e.target)) {
+                    return
+                }
+                this.showSignInDropdown = false;
+            }
+            document.addEventListener("click", listener)
+            this.$once("hook:beforeDestroy", () => {
+                document.removeEventListener("click", listener)
+            })
+        },
         data() {
             return {
                 showNav: false,
