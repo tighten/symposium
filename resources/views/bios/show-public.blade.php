bbf42884755a5dffaa19f553a5270f73c8d1846c
Andrew Morgan <andrewmile@gmail.com>
2020-05-01 13:35:56 -0500

    Update styles for public speaker bio

--- resources/views/bios/show-public.blade.php
+++ resources/views/bios/show-public.blade.php
@@ -2,20 +2,16 @@
 
 @section('content')
 
-<div class="body">
-    <div class="row">
-        <div class="col-md-8 col-md-push-2">
-            <p>
-                <a href="{{ route('speakers-public.show', ['profileSlug' => $user->profile_slug]) }}" class="btn btn-default">
-                    @svg('arrow-thick-left', 'w-4 mr-1 fill-current inline') Return to profile for {{ $user->name }}
-                </a>
-            </p><br>
+<div class="bg-white py-10 px-56 mt-8">
+    <p>
+        <a href="{{ route('speakers-public.show', ['profileSlug' => $user->profile_slug]) }}" class="bg-indigo-500 text-white rounded px-4 py-2 text-center">
+            @svg('arrow-thick-left', 'w-4 mr-1 fill-current inline') Return to profile for {{ $user->name }}
+        </a>
+    </p><br>
 
-            <h1 class="page-title">{{ $bio->nickname }}</h1>
+    <h2 class="text-4xl">{{ $bio->nickname }}</h2>
 
-            {!! str_replace("\n", "<br>", $bio->body) !!}
-        </div>
-    </div>
+    {!! str_replace("\n", "<br>", $bio->body) !!}
 </div>
 
 @endsection
