acd8c27dfae804d2dab0ec94a12595edf60b45d3
Andrew Morgan <andrewmile@gmail.com>
2020-05-08 12:21:49 -0500

    Update styles for public show conference page

--- resources/views/conferences/showPublic.blade.php
+++ resources/views/conferences/showPublic.blade.php
@@ -2,39 +2,38 @@
 
 @section('content')
 
-<div class="body">
-    <h1>{{ $conference->title }}</h1>
+<div class="max-w-md mx-auto sm:max-w-3xl border-2 border-indigo-200 rounded mt-4">
+    <div class="bg-white p-10">
+        <h2 class="m-0 font-sans text-2xl">
+            {{ $conference->title }}
+        </h2>
+        <div class="mt-8 font-sans">
+            @unless (empty($conference->location))
+                <div class="text-gray-500">Location:</div>
+                {{ $conference->location }}
+            @endunless
 
-    @unless (empty($conference->location))
-        <p><b>Location:</b>
-            {{ $conference->location }}</p>
-    @endunless
+            <div class="text-gray-500 mt-4">URL:</div>
+            <a href="{{ $conference->url }}">{{ $conference->url }}</a>
 
-    <p><b>Date created:</b>
-        {{ $conference->created_at->toFormattedDateString() }}</p>
-
-    <p><b>URL:</b>
-        <a href="{{ $conference->url }}">{{ $conference->url }}</a></p>
-
-    <p><b>Description:</b><br>
-        <!-- TODO: Figure out how we will be handling HTML/etc. -->
-        {!! str_replace("\n", "<br>", $conference->description) !!}</p>
-
-    <hr>
-
-    <div class="row">
-        <div class="col-md-6">
-            <p><b>Date conference starts:</b>
-                {{ $conference->startsAtDisplay() }}</p>
-
-            <p><b>Date conference ends:</b>
-                {{ $conference->endsAtDisplay() }}</p>
-
-            <p><b>Date CFP opens:</b>
-                {{ $conference->cfpStartsAtDisplay() }}</p>
-
-            <p><b>Date CFP closes:</b>
-                {{ $conference->cfpEndsAtDisplay() }}</p>
+            <div class="text-gray-500 mt-4">Description:</div>
+            {!! str_replace("\n", "<br>", $conference->description) !!}</p>
+        </div>
+    </div>
+    <div class="bg-indigo-150 px-10 py-3 font-sans flex justify-between">
+        <div>
+            <div class="text-gray-500">Dates</div>
+            <div>{{ $conference->startsAtDisplay() }} <span class="text-gray-500">to</span> {{ $conference->endsAtDisplay() }}</div>
+        </div>
+        @if ($conference->cfp_starts_at && $conference->cfp_ends_at)
+            <div>
+                <div class="text-gray-500">CFP</div>
+                <div>{{ $conference->cfpStartsAtDisplay() }} <span class="text-gray-500">to</span> {{ $conference->cfpEndsAtDisplay() }}</div>
+            </div>
+        @endif
+        <div>
+            <div class="text-gray-500">Created</div>
+            <div>{{ $conference->created_at->toFormattedDateString() }}</div>
         </div>
     </div>
 </div>
