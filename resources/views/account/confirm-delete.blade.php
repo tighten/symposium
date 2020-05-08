1c377948c9a2ec61e27263e291f7de0515345b8e
Andrew Morgan <andrewmile@gmail.com>
2020-05-08 11:18:40 -0500

    Update styles for delete account page

--- resources/views/account/confirm-delete.blade.php
+++ resources/views/account/confirm-delete.blade.php
@@ -2,12 +2,17 @@
 
 @section('content')
 
-<h2>Are you sure?</h2>
+<div class="bg-white py-10 px-10 lg:px-56 mt-8">
+    <h2 class="text-4xl mt-8">Are you sure?</h2>
 
-<p>Are you sure you want to delete your account?</p>
+    <p class="mt-8">Are you sure you want to delete your account?</p>
 
-{!! Form::open(array('action' => array('AccountController@destroy'))) !!}
-{!! Form::submit('Yes') !!} - <a href="{{ route('account.show') }}">No</a>
-{!! Form::close() !!}
+    {!! Form::open(['action' => ['AccountController@destroy']]) !!}
+        {!! Form::submit('Yes', [
+            'class' => 'bg-indigo-500 font-semibold mt-8 px-8 py-2 rounded text-white text-lg'
+        ]) !!}
+        <a href="{{ route('account.show') }}" class="border border-indigo-500 text-indigo-500 font-semibold mt-8 px-8 py-2 rounded text-lg inline-block">No</a>
+    {!! Form::close() !!}
+</div>
 
 @endsection
