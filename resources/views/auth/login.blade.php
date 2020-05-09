e924fac4bdfcca3fc79cdedcfbe4c27035b1573b
sariyana <cenitoria@tighten.co>
2020-05-09 15:25:40 -0500

    Update login page styles.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,20 +2,18 @@
 
 @section('content')
 
-<div class="row">
-    <div class="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <div class="panel panel-default panel-on-grey login-box">
-            <div class="panel-heading">
-                <h2 class="panel-title">Log in</h2>
+<div class="flex justify-center">
+    <div class="w-full md:w-2/3 bg-white my-12 shadow-md p-8 rounded">
+        <div class="panel-heading">
+            <h2 class="text-2xl text-center my-4">Log in</h2>
+        </div>
+        <div class="panel-body">
+            {!! $errors->first('auth', '<div class="alert alert-danger">:message</div>') !!}
+            <div class="text-center">
+                <a class="btn-github-login" href="{{ url('login/github') }}">Log in with <strong>GitHub</strong> @svg('github', 'inline-block align-top h-6 w-6')</a>
+                <p class="text-gray-400 text-base">or</p>
             </div>
-            <div class="panel-body">
-                {!! $errors->first('auth', '<div class="alert alert-danger">:message</div>') !!}
-                <div class="text-center">
-                    <a class="btn-github-login" href="{{ url('login/github') }}">Log in with <strong>GitHub</strong> @svg('github', 'inline-block align-top h-6 w-6')</a>
-                    <p class="text-gray-400 text-base">or</p>
-                </div>
-                @include('partials.log-in-form')
-            </div>
+            @include('partials.log-in-form')
         </div>
     </div>
 </div>
--- resources/views/partials/log-in-form.blade.php
+++ resources/views/partials/log-in-form.blade.php
@@ -7,22 +7,23 @@
 
     <div class="form-group">
         {!! Form::label('email', 'Email', ['class' => 'sr-only']) !!}
-        {!! Form::text('email', null, ['required', 'autofocus' => 'autofocus', 'class' => 'form-control', 'placeholder' => 'Email address']) !!}
+        {!! Form::text('email', null, ['required', 'autofocus' => 'autofocus', 'class' => 'form-input mt-1 block w-full rounded-lg', 'placeholder' => 'Email address']) !!}
         @if ($errors->loginForm->first('email'))
             <p class="mt-2 text-sm text-red-500 italic">{{ $errors->loginForm->first('email') }}</p>
         @endif
     </div>
 
-    <div class="form-group">
+    <div class="mt-2">
         {!! Form::label('password', 'Password', ['class' => 'sr-only']) !!}
-        {!! Form::password('password', ['required', 'class' => 'form-control', 'placeholder' => 'Password']) !!}
+        {!! Form::password('password', ['required', 'class' => 'form-input mt-1 block w-full rounded-lg', 'placeholder' => 'Password']) !!}
         @if ($errors->loginForm->first('password'))
             <p class="mt-2 text-sm text-red-500 italic">{{ $errors->loginForm->first('password') }}</p>
         @endif
     </div>
 
-    <div class="text-right">
-        <a href="/password/reset" class="btn btn-default">Reset Password</a>
-        {!! Form::submit('Log in', ['class' => 'btn btn-primary']) !!}
+    <div class="mt-8 md:flex md:justify-between">
+        {!! Form::submit('Log in', ['class' => 'bg-indigo block w-full mt-4 md:w-auto md:inline-block md:px-8 md:py-4 md:text-left lg:px-10 lg:py-5 px-4 py-2 rounded-lg text-center text-white whitespace-no-wrap']) !!}
+
+        <a href="/password/reset" class="block hover:border-indigo lg:ml-10 lg:mt-0 lg:px-10 lg:py-5 md:inline-block md:px-8 md:py-4 md:text-left mt-4 px-4 py-2 rounded rounded-lg text-center text-indigo whitespace-no-wrap">Reset Password</a>
     </div>
 {!! Form::close() !!}
