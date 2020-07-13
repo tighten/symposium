6670c61c9fe3b8a19e5951b333e5342b99bff931
Andrew Morgan <andrewmile@gmail.com>
2020-07-13 17:22:41 -0500

    Remove unused file

--- resources/views/partials/talkversionformheader.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="form-group">
-    {!! Form::label('nickname', '*Nickname for this talk version', ['class' => 'control-label']) !!}
-    {!! Form::text('nickname', $version->nickname, ['class' => 'form-control']) !!}
-
-    <span class="help-block">Each version of each talk has a nickname to distinguish it from the other versions. Something like "Extended Workshop" or "Business Owners version".</span>
-</div>
