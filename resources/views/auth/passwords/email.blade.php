07ca7db07e221717bc6af6d38f12701d0b817151
Andrew Morgan <andrewmile@gmail.com>
2020-07-14 12:02:04 -0500

    Update password reset pages with new designs

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,39 +1,29 @@
-@extends('layout')
+@extends('layout', ['title' => 'Reset Password'])
 
 @section('content')
 
-<div class="row">
-    <div class="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <div class="panel panel-default panel-on-grey login-box">
-            <div class="panel-heading">
-                <h2 class="panel-title">Reset Password</h2>
-            </div>
-            <div class="panel-body">
-                @include('partials.authentication-errors')
+@include('partials.authentication-errors')
 
-                <form method="POST" action="/password/email">
-                    {!! csrf_field() !!}
+<form method="POST" action="/password/email">
+    {!! csrf_field() !!}
 
-                    <x-input.text
-                        name="email"
-                        label="Email"
-                        type="email"
-                        placeholder="Email address"
-                        :hideLabel="true"
-                        autofocus="autofocus"
-                    ></x-input.text>
+    <x-input.text
+        name="email"
+        label="Email"
+        type="email"
+        placeholder="Email address"
+        :hideLabel="true"
+        autofocus="autofocus"
+        class="mt-4"
+    ></x-input.text>
 
-                    <x-button.primary
-                        type="submit"
-                        size="md"
-                        class="mt-8"
-                    >
-                        Send Password Reset Link
-                    </x-button.primary>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
+    <x-button.primary
+        type="submit"
+        size="md"
+        class="mt-8"
+    >
+        Send Password Reset Link
+    </x-button.primary>
+</form>
 
 @endsection
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,57 +1,48 @@
-@extends('layout')
+@extends('layout', ['title' => 'Reset Password'])
 
 @section('content')
 
-<div class="row">
-    <div class="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <div class="panel panel-default panel-on-grey login-box">
-            <div class="panel-heading">
-                <h2 class="panel-title">Reset Password</h2>
-            </div>
-            <div class="panel-body">
-                @include('partials.authentication-errors')
+@include('partials.authentication-errors')
 
-                <form method="POST" action="/password/reset">
-                    {!! csrf_field() !!}
-                    <input type="hidden" name="token" value="{{ $token }}">
+<form method="POST" action="/password/reset">
+    {!! csrf_field() !!}
+    <input type="hidden" name="token" value="{{ $token }}">
 
-                    <x-input.text
-                        name="email"
-                        label="Email"
-                        type="email"
-                        placeholder="Email address"
-                        :hideLabel="true"
-                        autofocus="autofocus"
-                    ></x-input.text>
+    <x-input.text
+        name="email"
+        label="Email"
+        type="email"
+        placeholder="Email address"
+        :hideLabel="true"
+        autofocus="autofocus"
+        class="mt-4"
+    ></x-input.text>
 
-                    <x-input.text
-                        name="password"
-                        label="Password"
-                        type="password"
-                        placeholder="Password"
-                        :hideLabel="true"
-                        class="mt-4"
-                    ></x-input.text>
+    <x-input.text
+        name="password"
+        label="Password"
+        type="password"
+        placeholder="Password"
+        :hideLabel="true"
+        class="mt-4"
+    ></x-input.text>
 
-                    <x-input.text
-                        name="password_confirmation"
-                        label="Password"
-                        type="password"
-                        placeholder="Confirm Password"
-                        :hideLabel="true"
-                        class="mt-4"
-                    ></x-input.text>
+    <x-input.text
+        name="password_confirmation"
+        label="Password"
+        type="password"
+        placeholder="Confirm Password"
+        :hideLabel="true"
+        class="mt-4"
+    ></x-input.text>
 
-                    <x-button.primary
-                        type="submit"
-                        size="md"
-                    >
-                        Reset Password
-                    </x-button.primary>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
+    <x-button.primary
+        type="submit"
+        size="md"
+        class="mt-8"
+    >
+        Reset Password
+    </x-button.primary>
+</form>
 
 @endsection
