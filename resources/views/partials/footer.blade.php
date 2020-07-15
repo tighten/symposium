2566e80679426864671a530304bc837b7e01ff2e
Andrew Morgan <andrewmile@gmail.com>
2020-07-15 13:52:34 -0500

    Different footer styles on non-home pages

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,16 +1,22 @@
 @php
-    $bg_color = $is_home ? 'bg-black' : 'bg-indigo-100';
+    $styles = $is_home ? 'bg-black' : 'bg-white border-t border-gray-300';
 @endphp
 
-<div class="{{ $bg_color }} pb-12 pt-4 bottom-0 absolute w-full">
+<div class="{{ $styles }} py-4 bottom-0 absolute w-full">
     <div class="font-sans text-lg text-gray-500 max-w-md mx-auto sm:max-w-6xl">
         <footer>
             <div class="flex flex-col lg:flex-row justify-between px-8 xl:px-0 text-center lg:text-left">
-                <div>&copy; <a href="http://tighten.co/">Tighten Co.</a> {{ date('Y') }}</div>
+                <div>
+                    <a href="http://tighten.co/">&copy; Tighten Co. {{ date('Y') }}</a>
+                </div>
                 <div class="invisible lg:visible">|</div>
-                <div>Source &amp; roadmap on <a href="https://github.com/tightenco/symposium">GitHub</a></div>
+                <div>
+                    <a href="https://github.com/tightenco/symposium">Source &amp; roadmap on GitHub</a>
+                </div>
                 <div class="invisible lg:visible">|</div>
-                <div>Submit your talks easily with <a href="http://rdohms.github.io/pronto/">Pronto!</a></div>
+                <div>
+                    <a href="http://rdohms.github.io/pronto/">Submit your talks easily with  Pronto!</a>
+                </div>
             </div>
         </footer>
     </div>
