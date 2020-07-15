404ff464c442966b4795144f7b29aced5d1ec466
Andrew Morgan <andrewmile@gmail.com>
2020-07-15 16:33:42 -0500

    Update styles for public talk page

--- resources/views/talks/show-public.blade.php
+++ resources/views/talks/show-public.blade.php
@@ -2,13 +2,14 @@
 
 @section('content')
 
-<div class="bg-white py-10 px-10 lg:px-56 mt-8">
-    <a
-        href="{{ route('speakers-public.show', $user->profile_slug) }}"
-        class="bg-indigo-500 text-white rounded px-4 py-2 text-center"
+<x-panel>
+    <x-button.primary
+        :href="route('speakers-public.show', $user->profile_slug)"
+        icon="arrow-thick-left"
+        class="inline-block"
     >
-        @svg('arrow-thick-left', 'w-4 mr-1 fill-current inline') Return to profile for {{ $user->name }}
-    </a>
+        Return to profile for {{ $user->name }}
+    </x-button.primary>
 
     <h2 class="text-4xl mt-8">{{ $talk->current()->title }}</h2>
 
@@ -19,6 +20,6 @@
     <h3 class="text-3xl mt-8">Description/Proposal</h3>
 
     {!! markdown($talk->current()->getDescription()) !!}
-</div>
+</x-panel>
 
 @endsection
