88a6213c221309f99a544673341bbf2051c0da12
Andrew Morgan <andrewmile@gmail.com>
2020-07-16 10:08:10 -0500

    Add missing styles for alerts

--- resources/views/partials/flash-messages.blade.php
+++ resources/views/partials/flash-messages.blade.php
@@ -1,26 +1,24 @@
-@if (Session::has('success-message') || Session::has('error-message') || Session::has('message'))
-    @if (Session::has('success-message'))
-        <div class="notice notice-success" onClick="$(this).remove();" data-dismiss="timeout">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-            {{ Session::get('success-message', 'success') }}
-        </div>
-    @endif
-    @if (Session::has('error-message'))
-        <div class="notice notice-error" onClick="$(this).remove();" data-dismiss="timeout">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-            {{ Session::get('error-message') }}
-        </div>
-    @endif
-    @if (Session::has('message'))
-        <div class="notice notice-info" onClick="$(this).remove();" data-dismiss="timeout">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-            {{ Session::get('message') }}
-        </div>
-    @endif
+@if (Session::has('success-message'))
+    <div class="notice notice-success" onClick="$(this).remove();" data-dismiss="timeout">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        {{ Session::get('success-message', 'success') }}
+    </div>
+@endif
+@if (Session::has('error-message'))
+    <div class="notice notice-error" onClick="$(this).remove();" data-dismiss="timeout">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        {{ Session::get('error-message') }}
+    </div>
+@endif
+@if (Session::has('message'))
+    <div class="notice notice-info" onClick="$(this).remove();" data-dismiss="timeout">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        {{ Session::get('message') }}
+    </div>
 @endif
