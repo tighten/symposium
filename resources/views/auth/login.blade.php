9759afb1997c82b5c8cc0d26e5e08844fec5ff08
Andrew Morgan <andrewmile@gmail.com>
2020-07-16 13:44:19 -0500

    Use panel component on login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,18 +2,20 @@
 
 @section('content')
 
-<div class="flex justify-center">
-    <div class="w-full md:w-2/3 bg-white my-12 shadow-md p-8 rounded">
-        <h2 class="text-2xl text-center my-4">Log in</h2>
-        <div class="panel-body">
-            {!! $errors->first('auth', '<div class="alert alert-danger">:message</div>') !!}
-            <div class="text-center">
-                <a class="btn-github-login" href="{{ url('login/github') }}">Log in with <strong>GitHub</strong> @svg('github', 'inline-block align-top h-6 w-6')</a>
-                <p class="text-gray-400 text-base">or</p>
-            </div>
-            @include('partials.log-in-form')
-        </div>
+<x-panel class="w-full md:w-2/3 mx-auto">
+    <h2 class="text-2xl text-center my-4">Log in</h2>
+    {!! $errors->first('auth', '<div class="alert alert-danger">:message</div>') !!}
+    <div class="text-center">
+        <a
+            class="btn-github-login"
+            href="{{ url('login/github') }}"
+        >
+            Log in with <strong>GitHub</strong>
+            @svg('github', 'inline-block align-top h-6 w-6')
+        </a>
+        <p class="text-gray-400 text-base my-2">or</p>
     </div>
-</div>
+    @include('partials.log-in-form')
+</x-panel>
 
 @endsection
