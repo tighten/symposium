62ef38e5ff57de1c7a76a0fbfa582850a4bb3d26
Andrew Morgan <andrewmile@gmail.com>
2020-07-17 14:15:36 -0500

    Remove copy bio button

--- resources/views/bios/listing.blade.php
+++ resources/views/bios/listing.blade.php
@@ -6,9 +6,6 @@
         <a href="{{ route('bios.delete', ['id' => $bio->id]) }}" class="ml-3" title="Delete">
             @svg('trash', 'w-5 fill-current inline')
         </a>
-        <button type="button" data-clipboard data-clipboard-text="{{ $bio->body }}" title="Copy" class="ml-3">
-            @svg('clipboard', 'w-5 fill-current inline')
-        </button>
     </x-slot>
     <x-slot name="body">{{ $bio->nickname }}</x-slot>
 </x-listing>
