ed6b5a6f75d1c7bfea40417899fdad76bb965f94
Marcy Acevedo <marcy@tighten.co>
2021-03-30 12:07:29 -0500

    Remove PurgeCSS from webpack and add it to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
+    purge: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
     theme: {
         fontSize: {
             ...defaultTheme.fontSize,
