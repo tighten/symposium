2bf093330c4f36ff6312b030b6dd3858fa0ba64c
Andrew Morgan <andrewmile@gmail.com>
2021-10-08 15:48:08 -0400

    Remove doc blocks from migrations

--- database/migrations/2019_10_25_152352_add_conference_location.php
+++ database/migrations/2019_10_25_152352_add_conference_location.php
@@ -6,11 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 class AddConferenceLocation extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::table('conferences', function (Blueprint $table) {
@@ -20,11 +15,6 @@ class AddConferenceLocation extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::table('conferences', function (Blueprint $table) {
