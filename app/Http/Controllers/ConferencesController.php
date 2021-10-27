a7d783a4a2eeaec0a00a9550e11c626a7524b39a
Andrew Morgan <andrewmile@gmail.com>
2021-10-27 16:24:39 -0400

    Replace the onOrAfter custom validator with after_or_equal

--- app/Http/Controllers/ConferencesController.php
+++ app/Http/Controllers/ConferencesController.php
@@ -20,7 +20,7 @@ class ConferencesController extends BaseController
         'location' => [],
         'cfp_url' => ['nullable', 'url'],
         'starts_at' => ['date'],
-        'ends_at' => ['date', 'onOrAfter:starts_at'],
+        'ends_at' => ['date', 'after_or_equal:starts_at'],
         'cfp_starts_at' => ['date', 'before:starts_at'],
         'cfp_ends_at' => ['date', 'after:cfp_starts_at', 'before:starts_at'],
     ];
--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -16,6 +16,7 @@ return [
     'accepted'             => 'The :attribute must be accepted.',
     'active_url'           => 'The :attribute is not a valid URL.',
     'after'                => 'The :attribute must be a date after :date.',
+    'after_or_equal'       => 'The :attribute must be a date after or equal to :date.',
     'alpha'                => 'The :attribute may only contain letters.',
     'alpha_dash'           => 'The :attribute may only contain letters, numbers, and dashes.',
     'alpha_num'            => 'The :attribute may only contain letters and numbers.',
