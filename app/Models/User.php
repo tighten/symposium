b6a4720b30b629770b1339828698ba182118a7e9
Andrew Morgan <andrew@tighten.co>
2022-07-08 14:22:38 -0400

    Remove unused method

--- app/Models/User.php
+++ app/Models/User.php
@@ -73,13 +73,6 @@ class User extends Authenticatable
             ->archived();
     }
 
-    public function getActiveTalksAttribute()
-    {
-        return $this->talks()->get()->sortBy(function ($talk) {
-            return strtolower($talk->current()->title);
-        })->values();
-    }
-
     public function bios()
     {
         return $this->hasMany(Bio::class)->orderBy('nickname');
