72ed82d4d432bc3b535529b474d2536976d07b8b
Andrew Morgan <andrew@tighten.co>
2022-07-14 15:24:21 -0400

    Remove unused jquery bindings

--- resources/js/app.js
+++ resources/js/app.js
@@ -59,21 +59,6 @@ new Vue({
 
 // jQuery bindings
 $(function() {
-    $('[data-confirm]').on('click', function( e ) {
-        if (! confirm($(this).attr('data-confirm'))) {
-            e.preventDefault();
-            e.cancelBubble = true;
-        }
-    });
-
-    $('[data-toggle=collapse]').on('click', function( e ) {
-        var target = $(this).attr('data-target');
-
-        e.preventDefault();
-
-        $(target).toggle();
-    });
-
     $('[data-dismiss=timeout]').each(function() {
         var timeout_len = 2000,
             $dismiss_target = $(this);
@@ -85,27 +70,5 @@ $(function() {
 
     $('input[type=date]').pickadate({
         format: 'yyyy-mm-dd'
-    });
-
-    $('.bio-modal').on('shown.bs.modal', function () {
-        $(this).find('textarea').focus();
-    });
-
-    $('[data-clipboard]').each(function (i, element) {
-        $(element).tooltip({
-            trigger: 'manual',
-            placement: 'bottom',
-            title: 'Copied!'
-        });
-
-        var client = new ZeroClipboard(element);
-        client.on("ready", function(readyEvent) {
-            client.on("aftercopy", function(event) {
-                $(element).tooltip('show');
-                setTimeout(function () {
-                    $(element).tooltip('hide');
-                }, 800);
-            } );
-        });
     });
 });
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,7 +13,7 @@
     <meta property="og:type" content="website">
     <meta property="og:url" content="https://symposiumapp.com/">
     <meta property="og:image" content="{{ asset('img/symposium-banner.png') }}">
-    
+
     <meta property="twitter:card" content="summary_large_image">
     <meta property="twitter:url" content="https://symposiumapp.com/">
     <meta property="twitter:title" content="{{ config('app.name') }}">
@@ -59,7 +59,6 @@
     </div>
 
     <script src="//cdn.jsdelivr.net/jquery/2.1.3/jquery.min.js"></script>
-    <script src="//cdn.jsdelivr.net/zeroclipboard/2.2.0/ZeroClipboard.min.js"></script>
     <script src="https://maps.googleapis.com/maps/api/js?key={{ config('services.google.maps.key') }}&libraries=places"></script>
     <script src="{{ ('/js/vendor/pickadate/picker.js') }}"></script>
     <script src="{{ ('/js/vendor/pickadate/picker.date.js') }}"></script>
