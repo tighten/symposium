e163f25764076ad79971cabb845743c4d8e44760
Cenitoria Melton <cenitoria.melton@tighten.co>
2022-07-27 14:33:34 -0500

    Remove unused imports.

--- tests/Feature/SpeakerPackageTest.php
+++ tests/Feature/SpeakerPackageTest.php
@@ -4,8 +4,6 @@ namespace Tests\Feature;
 
 use App\Models\Conference;
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class SpeakerPackageTest extends TestCase
