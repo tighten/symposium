cec7b3d7456d8bdec10f9d034a6e8ac51ad7c32a
Cenitoria Melton <cenitoria.melton@tighten.co>
2022-08-05 18:47:33 -0500

    Display currency symbol as input label.

--- resources/js/components/SpeakerPackage.vue
+++ resources/js/components/SpeakerPackage.vue
@@ -12,26 +12,26 @@
                 name="speaker_package[currency]"
                 id="currency"
                 :value="selectedCurrency"
-                @change="(e) => selectedCurrency = e.target.value"
+                @change="(e) => updateSelectedCurrency(e.target.value)"
             >
                 <option v-for="currency in currencies" :value="currency.code" :key="currency.code">
-                    {{ currency.symbol || currency.code }}
+                    {{ currency.code }}
                 </option>
             </select>
     </div>
 
     <div class="flex mt-2">
-        <span class="p-2 bg-indigo-300 rounded-l-md">{{ selectedCurrency }}</span>
+        <span class="p-2 bg-indigo-300 rounded-l-md">{{ selectedCurrencySymbol }}</span>
         <input name="speaker_package[travel]" :value="speakerPackage && speakerPackage.travel"  class="bg-white border-form-200 form-input placeholder-form-400 rounded-r-md">
     </div>
     
     <div class="flex mt-2">
-        <span class="p-2 bg-indigo-300 rounded-l-md">{{ selectedCurrency }}</span>
+        <span class="p-2 bg-indigo-300 rounded-l-md">{{ selectedCurrencySymbol }}</span>
         <input name="speaker_package[hotel]" :value="speakerPackage && speakerPackage.hotel" class="bg-white border-form-200 form-input placeholder-form-400 rounded-r-md">
     </div>
     
     <div class="flex mt-2">
-        <span class="p-2 bg-indigo-300 rounded-l-md">{{ selectedCurrency }}</span>
+        <span class="p-2 bg-indigo-300 rounded-l-md">{{ selectedCurrencySymbol }}</span>
         <input name="speaker_package[food]" :value="speakerPackage && speakerPackage.food" class="bg-white border-form-200 form-input placeholder-form-400 rounded-r-md">
     </div>
 </div>
@@ -49,7 +49,17 @@ export default {
     data() {
         return {
             selectedCurrency: this.initialCurrency,
+            selectedCurrencySymbol: this.initialCurrency,
         };
+    },
+    mounted() {
+        this.selectedCurrencySymbol = Object.values(this.currencies).find(({code}) => code == this.initialCurrency).symbol
+    },
+    methods: {
+        updateSelectedCurrency(e) {
+            this.selectedCurrency = e;
+            this.selectedCurrencySymbol = Object.values(this.currencies).find(({code}) => code == e).symbol;
+        }
     },
 }
 </script>
