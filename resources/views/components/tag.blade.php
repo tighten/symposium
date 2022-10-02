f1885d0131ccff813b0d51f73ea3e51068b00ce6
Mo Sureerat <sureemo@gmail.com>
2022-10-02 14:03:33 +0700

    No line break on Tag

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,5 +1,5 @@
 <span
-    class="px-1 ml-4 text-xs font-semibold text-white bg-indigo-500 rounded"
+    class="px-1 ml-4 text-xs font-semibold text-white bg-indigo-500 rounded whitespace-nowrap"
 >
     {{ $slot }}
 </span>
