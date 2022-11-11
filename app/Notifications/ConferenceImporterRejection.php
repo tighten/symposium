c0484091c7ea2e0833f3f2cce94aebd50733b4f9
Andrew Morgan <andrew@tighten.co>
2022-11-11 16:00:10 -0500

    Import missing class

--- app/Notifications/ConferenceImporterRejection.php
+++ app/Notifications/ConferenceImporterRejection.php
@@ -5,6 +5,7 @@ namespace App\Notifications;
 use App\Models\Conference;
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
+use Illuminate\Support\Carbon;
 
 class ConferenceImporterRejection extends Notification
 {
