fff7636f179a20f0e45d4757a0d2d6e3f4c8289c
Matt Stauffer <matt@tighten.co>
2022-11-12 23:01:09 -0500

    Temporarily disable calling all papers sync

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,8 +15,8 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('callingallpapers:sync')
-            ->hourly();
+        // $schedule->command('callingallpapers:sync')
+        //     ->hourly();
 
         // $schedule->command('tweet:cfpDates')
         //      ->dailyAt('08:00')
