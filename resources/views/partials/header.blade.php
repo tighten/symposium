5c7ea2b3355535352e4539b47b27206cdaca1758
Andrew Morgan <andrew@tighten.co>
2023-04-06 16:05:48 +0900

    Update navbar styles

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,18 +1,8 @@
 @include('partials.flash-messages')
 <div class="bg-indigo-600">
-    <main-header class="max-w-xl h-16 px-4 mx-auto sm:max-w-6xl flex items-center justify-between">
+    <main-header class="max-w-xl h-16 px-2 sm:px-0 mx-auto sm:max-w-7xl flex items-center justify-between">
         <template slot-scope="slotProps">
-            <div class="sm:px-4 flex items-center justify-between">
-                <a href="{{ auth()->check() ? route('dashboard') : url('/') }}">
-                    @svg('logo')
-                </a>
-                @include('partials.nav')
-                <div class="lg:hidden">
-                    <button v-on:click="slotProps.toggleNav" type="button" class="mobileMenuBtn focus:outline-none transform-gpu">
-                        <div :class="slotProps.showNav ? 'invisible h-0 bg-opacity-0' : 'visible w-6 h-0.5 bg-indigo-800 bg-opacity-100'"></div>
-                    </button>
-                </div>
-            </div>
+            @include('partials.nav')
             @if (auth()->guest())
                 <div class="flex my-4 sm:ml-2 lg:mb-0 lg:mt-0 lg:block">
                     <a class="inline-block px-8 py-2 mt-4 text-indigo-800 transition duration-150 ease-in-out border rounded border-indigo hover:text-white hover:bg-indigo-800 md:ml-2 lg:ml-4 lg:block lg:mt-0 lg:px-4" href="#" v-on:click="slotProps.toggleSignInDropdown">
@@ -24,40 +14,66 @@
                     </div>
                 </div>
             @else
-                <div class="lg:mt-0">
+                <div class="lg:mt-0 px-4 relative">
                     <a class="items-center lg:mt-0" href="#" v-on:click="slotProps.toggleAccountDropdown">
                         <img
                             src="{{ auth()->user()->profile_picture_thumb }}"
-                            class="hidden inline w-12 rounded-full lg:block"
+                            class="hidden inline w-8 rounded-full lg:block"
                         >
                     </a>
-                    <div class="absolute z-50 flex flex-col py-1 mt-2 bg-white border rounded lg:mr-4 lg:mx-2 md:mx-4 border-indigo lg:right-0" :class="slotProps.showAccountDropdown ? 'block' : 'hidden'">
-                        <a class="px-4 py-1 hover:bg-indigo-100 hover:text-indigo-500" href="{{ route('account.show') }}">Account</a>
+                    <div
+                        class="bg-white border border-indigo-600 flex flex-col py-1 mt-6 right-0 rounded text-indigo-600 z-50"
+                        :class="slotProps.showAccountDropdown ? 'absolute' : 'hidden'"
+                    >
+                        <a
+                            href="{{ route('account.show') }}"
+                            class="px-4 py-1 hover:bg-indigo-600 hover:text-white"
+                        >
+                            Account
+                        </a>
                         @if (auth()->user()->enable_profile)
                             <a
-                                class="px-4 py-1 whitespace-nowrap hover:bg-indigo-100 hover:text-indigo-500"
                                 href="{{ route('speakers-public.show', auth()->user()->profile_slug) }}"
+                                class="px-4 py-1 whitespace-nowrap hover:bg-indigo-600 hover:text-white"
                             >
                                 Public Speaker Profile
                             </a>
                         @endif
-                        <a class="px-4 py-1 hover:bg-indigo-100 hover:text-indigo-500" href="{{ route('log-out') }}">Log out</a>
+                        <a
+                            href="{{ route('log-out') }}"
+                            class="px-4 py-1 hover:bg-indigo-600 hover:text-white"
+                        >
+                            Log out
+                        </a>
                     </div>
                 </div>
             @endif
-            @if ($title)
-                @php
-                    $app_header = $is_conferences ? 'sm:max-w-5xl' : 'sm:max-w-3xl'
-                @endphp
-                <div class="max-w-md mx-auto mt-3 border-t border-gray-300 sm:max-w-6xl">
-                    <div class="mx-auto {{ $app_header }}">
-                        <h2 class="mt-3 font-sans text-2xl text-black">{{ $title }}</h2>
-                    </div>
-                </div>
-            @endif
+            <div
+                class="lg:hidden"
+                :class="slotProps.showNav ? 'mr-3' : ''"
+            >
+                <button
+                    v-on:click="slotProps.toggleNav"
+                    type="button"
+                    class="mobileMenuBtn focus:outline-none transform-gpu"
+                >
+                    <div :class="slotProps.showNav ? 'invisible h-0 bg-opacity-0' : 'visible w-6 h-0.5 bg-white bg-opacity-100'"></div>
+                </button>
+            </div>
         </template>
     </main-header>
 </div>
+
+@if ($title)
+    @php
+        $app_header = $is_conferences ? 'sm:max-w-5xl' : 'sm:max-w-3xl'
+    @endphp
+    <div class="max-w-md mx-auto mt-3 border-t border-gray-300 sm:max-w-6xl">
+        <div class="mx-auto {{ $app_header }}">
+            <h2 class="mt-3 font-sans text-2xl text-black">{{ $title }}</h2>
+        </div>
+    </div>
+@endif
 
 
 
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,60 +1,42 @@
-<nav :class="slotProps.showNav ? 'block' : 'hidden'" class="absolute left-0 items-center block w-full font-sans font-semibold text-white shadow-sm text-sm leading-5 font-medium lg:w-auto lg:relative lg:shadow-none lg:flex">
+<nav
+    class="bg-indigo-600 block gap-4 text-sm text-white lg:flex lg:items-center"
+    :class="slotProps.showNav ? 'absolute w-full top-4' : ''"
+>
     @if (auth()->check())
         @php
             $menuItems = [
-                [
-                    'title' => 'Dashboard',
-                    'url' => route('dashboard'),
-                    'classes' => 'font-medium ml-0 lg:mr-2 sm:mx-2 md:mx-4',
-                ],
-                [
-                    'title' => 'Bios',
-                    'url' => route('bios.index'),
-                    'classes' => 'font-medium lg:mr-2 sm:mx-2 md:mx-4 mt-2 lg:mt-0',
-                ],
-                [
-                    'title' => 'Conferences',
-                    'url' => route('conferences.index'),
-                    'classes' => 'font-medium lg:mr-2 sm:mx-2 md:mx-4 mt-2 lg:mt-0',
-                ],
-                [
-                    'title' => 'Calendar',
-                    'url' => route('calendar.index'),
-                    'classes' => 'font-medium lg:mr-2 sm:mx-2 md:mx-4 mt-2 lg:mt-0',
-                ],
-                [
-                    'title' => 'Talks',
-                    'url' => route('talks.index'),
-                    'classes' => 'font-medium lg:mr-2 sm:ml-2 md:ml-4 mt-2 lg:mt-0',
-                ],
+                ['route' => 'dashboard', 'title' => 'Dashboard'],
+                ['route' => 'bios.index', 'title' => 'Bios'],
+                ['route' => 'conferences.index', 'title' => 'Conferences'],
+                ['route' => 'calendar.index', 'title' => 'Calendar'],
+                ['route' => 'talks.index', 'title' => 'Talks'],
 
             ];
         @endphp
     @else
         @php
             $menuItems = [
-                [
-                    'title' => 'How it Works',
-                    'url' => url('what-is-this'),
-                    'classes' => 'font-medium ml-0 lg:mr-2 sm:mx-2 md:mx-4',
-                ],
-                [
-                    'title' => 'Our speakers',
-                    'url' => url('speakers'),
-                    'classes' => 'font-medium lg:mr-2 sm:mx-2 md:mx-4 mt-2 lg:mt-0',
-                ],
-                [
-                    'title' => 'Conferences',
-                    'url' => route('conferences.index'),
-                    'classes' => 'font-medium lg:mr-2 sm:mx-2 md:mx-4 sm:mr-0 mt-2 lg:mt-0',
-                ],
+                ['route' => 'what-is-this', 'title' => 'How it Works'],
+                ['route' => 'speakers-public.index', 'title' => 'Our speakers'],
+                ['route' => 'conferences.index', 'title' => 'Conferences'],
             ];
         @endphp
     @endif
 
+    <a
+        href="{{ auth()->check() ? route('dashboard') : url('/') }}"
+        class="block px-4"
+        :class="slotProps.showNav ? 'pb-4' : ''"
+    >
+        @svg('logo')
+    </a>
+
     @foreach ($menuItems as $item)
-        <a class="block border-b border-indigo hover:text-indigo-500 hover:underline border-solid lg:border-none uppercase {{ $item['classes'] }}"
-           href="{{ $item['url'] }}"
+        <a
+            href="{{ route($item['route']) }}"
+            :class="slotProps.showNav ? 'block py-4' : 'hidden lg:block py-2'"
+            class="px-3 rounded hover:underline
+            @if (request()->isContainedBy($item['route'])) bg-indigo-700 @endif"
         >
             {{ $item['title'] }}
         </a>
