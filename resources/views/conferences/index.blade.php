7f2c20f70de018e1416110a01f1075787b0cc6a7
Andrew Morgan <andrew@tighten.co>
2023-05-03 06:55:26 +0900

    Remove unused sort links

--- resources/views/conferences/index.blade.php
+++ resources/views/conferences/index.blade.php
@@ -1,30 +1,5 @@
 @extends('app', ['title' => 'Conferences'])
 
-@php
-    $sortLinks = [
-        'closing_next' => [
-            'label' => 'CFP Closing Next',
-            'route' => 'conferences.index',
-            'query' => ['sort' => 'closing_next'],
-        ],
-        'opening_next' => [
-            'label' => 'CFP Opening Next',
-            'route' => 'conferences.index',
-            'query' => ['sort' => 'opening_next'],
-        ],
-        'alpha' => [
-            'label' => 'Title',
-            'route' => 'conferences.index',
-            'query' => ['sort' => 'alpha'],
-        ],
-        'date' => [
-            'label' => 'Date',
-            'route' => 'conferences.index',
-            'query' => ['sort' => 'date'],
-        ],
-    ];
-@endphp
-
 @section('content')
     <div>
         <livewire:conference-list/>
