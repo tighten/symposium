13356f3ce575162d7561d28b4d895e37042b103d
Andrew Morgan <andrew@tighten.co>
2024-06-07 16:32:35 -0400

    Add test coverage for authenticating socialite users

--- app/Models/UserSocial.php
+++ app/Models/UserSocial.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class UserSocial extends Model
 {
+    use HasFactory;
+
     protected $table = 'users_social';
 
     protected $fillable = ['social_id', 'service'];
