ea84d2dbc6a6a5c0e0c68620c46bdb2b831cbab0
Andrew Morgan <andrew@tighten.co>
2024-06-28 18:21:41 -0400

    Add missing test coverage for unauthorized api call

--- tests/Api/MeApiTest.php
+++ tests/Api/MeApiTest.php
@@ -2,15 +2,29 @@
 
 namespace Tests\Api;
 
-class MeApiTest extends ApiTestCase
+use App\Models\User;
+use Laravel\Passport\Passport;
+use Tests\TestCase;
+
+class MeApiTest extends TestCase
 {
     /** @test */
     public function can_fetch_my_info()
     {
+        Passport::actingAs(User::factory()->create());
+
         $response = $this->call('GET', 'api/me');
         $data = json_decode($response->getContent());
 
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertIsObject($data->data);
+    }
+
+    /** @test */
+    public function only_authenticated_users_can_fetch_user_data(): void
+    {
+        $response = $this->getJson('api/me');
+
+        $response->assertUnauthorized();
     }
 }
