9a942b85b5e25aae1f7c3342345a142c7622af88
Shift <shift@laravelshift.com>
2024-07-02 14:58:41 +0000

    Update method signatures for Laravel contracts

--- app/Casts/Url.php
+++ app/Casts/Url.php
@@ -2,16 +2,17 @@
 
 namespace App\Casts;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 
 class Url implements CastsAttributes
 {
-    public function get($model, $key, $value, $attributes)
+    public function get(Model $model, string $key, mixed $value, array $attributes)
     {
         return ($value && ! str_contains($value, 'http')) ? "https://{$value}" : $value;
     }
 
-    public function set($model, $key, $value, $attributes)
+    public function set(Model $model, string $key, mixed $value, array $attributes)
     {
         return ($value && ! str_contains($value, 'http')) ? "https://{$value}" : $value;
     }
