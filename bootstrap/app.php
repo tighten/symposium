e21dfbd5e391036fa0fc24b8414741134b25a895
Andrew Morgan <andrew@tighten.co>
2024-07-03 10:11:05 -0400

    Import class

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Foundation\Application;
+
 /*
 |--------------------------------------------------------------------------
 | Create The Application
@@ -11,7 +13,7 @@
 |
 */
 
-$app = new Illuminate\Foundation\Application(
+$app = new Application(
     realpath(__DIR__ . '/../')
 );
 
