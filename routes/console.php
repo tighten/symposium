c1bb518ee3a1f898b9776a61bbb4fdb7d312d363
Shift <shift@laravelshift.com>
2024-07-19 14:06:07 +0000

    Re-register scheduled commands

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,24 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+Schedule::command('callingallpapers:sync')
+    ->hourly();
+
+// Schedule::command('tweet:cfpDates')
+//      ->dailyAt('08:00')
+//      ->environments('production');
+
+Schedule::command('symposium:notifyCfps')
+    ->dailyAt('08:00')
+    ->environments('production');
+
+Schedule::command('verify-conference-importer-heartbeat')
+    ->dailyAt('10:00')
+    ->environments('production');
