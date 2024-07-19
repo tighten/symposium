730e0cf82b6482cf64197229b9ef6abe87c56ea5
Shift <shift@laravelshift.com>
2024-07-19 14:06:02 +0000

    Set new `ENV` variables

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
+            'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
