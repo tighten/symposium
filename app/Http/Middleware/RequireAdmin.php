a343509341d519ed74cff8a733921853190a64a7
Andrew Morgan <andrew@tighten.co>
2024-11-15 19:30:14 -0500

    Revert "Delete unused class"
    This reverts commit d130e8f1ad5bcab515b9cc444de02ca710d207ac.

---
+++ app/Http/Middleware/RequireAdmin.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class RequireAdmin
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (! $request->user()?->isAdmin()) {
+            throw new NotFoundHttpException;
+        }
+
+        return $next($request);
+    }
+}
