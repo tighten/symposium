a652d85cd80fda9491cf4198afff9e0b823e4c25
Andrew Morgan <andrew@tighten.co>
2025-02-14 13:31:06 -0500

    Add return type

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\View\View;
 use Intervention\Image\Facades\Image;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class AccountController extends Controller
 {
@@ -95,7 +96,7 @@ class AccountController extends Controller
         return view('account.confirm-delete');
     }
 
-    public function export()
+    public function export(): BinaryFileResponse
     {
         $user = auth()->user();
         $user->load([
