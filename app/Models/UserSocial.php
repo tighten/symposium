fb632eebd1295d5709b5fe44bc4ec77f418505c8
Andrew Morgan <andrew@tighten.co>
2025-03-14 11:45:00 -0400

    Remove unused relationship

--- app/Models/UserSocial.php
+++ app/Models/UserSocial.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserSocial extends Model
 {
@@ -13,9 +12,4 @@ class UserSocial extends Model
     protected $table = 'users_social';
 
     protected $fillable = ['social_id', 'service'];
-
-    public function user(): BelongsTo
-    {
-        return $this->belongsTo(User::class);
-    }
 }
