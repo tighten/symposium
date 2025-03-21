12d8717a8685fa44078df359be6fa136c23250c3
Andrew Morgan <andrew@tighten.co>
2025-03-21 11:06:31 -0400

    Remove unused dependency

--- app/CallingAllPapers/Client.php
+++ app/CallingAllPapers/Client.php
@@ -2,18 +2,10 @@
 
 namespace App\CallingAllPapers;
 
-use GuzzleHttp\Client as GuzzleClient;
 use Illuminate\Support\Facades\Http;
 
 class Client
 {
-    private $guzzle;
-
-    public function __construct(GuzzleClient $client)
-    {
-        $this->guzzle = $client;
-    }
-
     public function getEvents()
     {
         return collect(json_decode($this->get())->cfps)->map(function ($cfpFromApi) {
